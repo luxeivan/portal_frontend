3ffc102c1ba29b8487edcda3e36971fb2f024f6f
Stanislav <yanut995@gmail.com>
2024-02-29 16:42:29 +0300

    Added validation number

--- src/components/Global/Auth/PhoneVerification/PhoneVerification.js
+++ src/components/Global/Auth/PhoneVerification/PhoneVerification.js
@@ -1,42 +1,55 @@
 import React from "react";
 import { Input, Button, Form, Typography } from "antd";
 import useRegistration from "../../../../stores/useRegistration";
-import { PhoneOutlined, SearchOutlined } from "@ant-design/icons";
-import styles from "./PhoneVerification.module.css";
+import { PhoneOutlined } from "@ant-design/icons";
 
-const { Title, Text, Paragraph } = Typography;
+const { Paragraph } = Typography;
 
 const PhoneVerification = () => {
+  const [form] = Form.useForm();
   const submitPhone = useRegistration((state) => state.submitPhone);
 
   const onFinish = (values) => {
-    submitPhone(values.phone);
+    submitPhone(values.phone);     
   };
+  
+  const validatePhone = (_, value) => {
+    const regex = /^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
+    if (regex.test(value)) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('Введите номер телефона в формате +7(***)***-**-**'));
+  };
+
+  const isPhoneValid = !form.getFieldError('phone').length && form.isFieldTouched('phone');
 
   return (
     <div>
-    <Form onFinish={onFinish}>
-      <Paragraph>
-        Укажите номер мобильного телефона в формате +7(***)***-**-**.
-      </Paragraph>
-      <Form.Item
-        name="phone"
-        rules={[
-          {
-            required: true,
-            message: "Пожалуйста, введите ваш номер телефона!",
-          },
-        ]}
-      >
-        <Input addonBefore={<PhoneOutlined />} placeholder="Номер телефона" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Получить код
-        </Button>
-      </Form.Item>
-    </Form>
-    <Button type="link">*Инструкция по регистрации</Button>
+      <Form form={form} onFinish={onFinish}>
+        <Paragraph>
+          Укажите номер мобильного телефона в формате +7(***)***-**-**.
+        </Paragraph>
+        <Form.Item
+          name="phone"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите ваш номер телефона!",
+            },
+            {
+              validator: validatePhone,
+            },
+          ]}
+        >
+          <Input addonBefore={<PhoneOutlined />} placeholder="+7(***)***-**-**" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" disabled={!isPhoneValid}>
+            Получить код
+          </Button>
+        </Form.Item>
+      </Form>
+      <Button type="link">*Инструкция по регистрации</Button>
     </div>
   );
 };
