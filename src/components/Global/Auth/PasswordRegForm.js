6628b472f6ee3999f227843b688fd3d1b413e412
Stanislav <yanut995@gmail.com>
2024-03-05 11:45:45 +0300

    Added Step 3, and wait Ivan

--- src/components/Global/Auth/PasswordRegForm.js
+++ src/components/Global/Auth/PasswordRegForm.js
@@ -1,7 +1,121 @@
-import React from 'react'
+// import React from "react";
+// import { Button, Form, Input, Typography } from "antd";
+// const { Paragraph } = Typography;
+
+// export default function PasswordRegForm() {
+//   return (
+//     <div>
+//       <Form>
+//         <Paragraph>
+//           {" "}
+//           Пароль должен содержать минимум 8 символов, заглавную букву, цифры,
+//           латинские буквы.
+//         </Paragraph>
+//         <Form.Item
+//           label="Пароль"
+//           name="password"
+//           rules={[
+//             {
+//               required: true,
+//               message: "Это поле обязательно",
+//             },
+//             {
+//               min: 10,
+//               message: "Минимальная длина пароля 10 символов",
+//             },
+//           ]}
+//         >
+//           <Input.Password />
+//         </Form.Item>
+//         <Form.Item
+//           label="Повторите пароль"
+//           name="repeat__password"
+//           rules={[
+//             {
+//               required: true,
+//               message: "Это поле обязательно",
+//             },
+//             {
+//               min: 10,
+//               message: "Минимальная длина пароля 10 символов",
+//             },
+//           ]}
+//         >
+//           <Input.Password />
+//         </Form.Item>
+//         <Form.Item>
+//           <Button type="primary" htmlType="submit">
+//             Сохранить
+//           </Button>
+//         </Form.Item>
+//       </Form>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import { Button, Form, Input, Typography } from "antd";
+import useRegistration from "../../../stores/useRegistration"; // Путь может быть другим, в зависимости от вашей структуры проекта
+
+const { Paragraph } = Typography;
 
 export default function PasswordRegForm() {
+  const [form] = Form.useForm();
+  const { registerUser } = useRegistration();
+
+  const onFinish = (values) => {
+    if (values.password !== values.repeat_password) {
+      console.error("Пароли не совпадают.");
+      return;
+    }
+    registerUser(values.password);
+  };
+
   return (
-    <div>PasswordRegForm</div>
-  )
+    <div>
+      <Form form={form} onFinish={onFinish}>
+        <Paragraph>
+          Пароль должен содержать минимум 8 символов, заглавную букву, цифры,
+          латинские буквы.
+        </Paragraph>
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Это поле обязательно",
+            },
+            {
+              min: 10,
+              message: "Минимальная длина пароля 10 символов",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Повторите пароль"
+          name="repeat_password"
+          rules={[
+            {
+              required: true,
+              message: "Это поле обязательно",
+            },
+            {
+              min: 10,
+              message: "Минимальная длина пароля 10 символов",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Зарегистрироваться
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
--- src/stores/useRegistration.js
+++ src/stores/useRegistration.js
@@ -1,3 +1,115 @@
+// import { create } from "zustand";
+// import axios from "axios";
+// import config from "../config";
+
+// const useRegistration = create((set, get) => ({
+//   registrationStep: 0,
+//   phone: "",
+//   email: "",
+//   phoneCode: "",
+//   emailCode: "",
+//   password: "",
+//   phoneVerified: false,
+//   codeRequested: false,
+//   emailVerified: false,
+//   codeRequestedEmail: false, 
+
+//   setRegistrationStep: (step) => set(() => ({ registrationStep: step })),
+//   setPhone: (phone) => set(() => ({ phone })),
+//   setEmail: (email) => set(() => ({ email })),
+//   setPhoneVerified: (verified) => set(() => ({ phoneVerified: verified })),
+//   setEmailVerified: (verified) => set(() => ({ emailVerified: verified })),
+
+//   submitPhone: async (phone) => {
+//     try {
+//       const response = await axios.post(
+//         `${config.backServer}/api/registration/phone`,
+//         { phone },
+//         { withCredentials: true }
+//       );
+//       if (response.data.status === "ok") {
+//         set(() => ({
+//           phone,
+//           phoneSubmitted: true,
+//           codeRequested: true,
+//         }));
+//       } else {
+//         console.error(response.data.message);
+//       }
+//     } catch (error) {
+//       console.error("Ошибка при отправке номера телефона", error);
+//     }
+//   },
+
+//   submitPhoneCode: async (phoneCode) => {
+//     try {
+//       const response = await axios.post(
+//         `${config.backServer}/api/registration/phonecode`,
+//         { phoneCode },
+//         { withCredentials: true }
+//       );
+//       if (response.data.status === "ok") {
+//         get().setPhoneVerified(true);
+//         set(() => ({
+//           registrationStep: 1,
+//           codeRequested: false
+//         }));
+//       } else {
+//         console.error("Неверный пин-код");
+//       }      
+//     } catch (error) {
+//       console.error("Ошибка при подтверждении пин-кода", error);
+//     }
+//   },
+
+//   submitEmail: async (email) => {
+//     try {
+//       const response = await axios.post(
+//         `${config.backServer}/api/registration/email`,
+//         { email },
+//         { withCredentials: true }
+//       );
+//       if (response.data.status === "ok") {
+//         set(() => ({
+//           email,
+//           emailSubmitted: true,
+//           codeRequestedEmail: true,
+//         }));
+//         // return response.data;
+//       } else {
+//         console.error(response.data.message);
+//         // return response.data;
+//       }
+//     } catch (error) {
+//       console.error("Ошибка при отправке email", error);
+//       // return { status: 'error', message: "Ошибка при отправке email" };
+//     }
+//   },
+
+//   submitEmailCode: async (emailCode) => {
+//     try {
+//       const response = await axios.post(
+//         `${config.backServer}/api/registration/emailcode`,
+//         { emailCode },
+//         { withCredentials: true }
+//       );
+//       if (response.data.status === "ok") {
+//         get().setEmailVerified(true);
+//         set(() => ({
+//           registrationStep: 2,
+//           codeRequestedEmail: false
+//         }));
+//       } else {
+//         console.error("Неверный пин-код");
+//       }      
+//     } catch (error) {
+//       console.error("Ошибка при подтверждении пин-кода", error);
+//     }
+//   },
+// }));
+
+// export default useRegistration;
+
 import { create } from "zustand";
 import axios from "axios";
 import config from "../config";
@@ -75,14 +187,11 @@ const useRegistration = create((set, get) => ({
           emailSubmitted: true,
           codeRequestedEmail: true,
         }));
-        // return response.data;
       } else {
         console.error(response.data.message);
-        // return response.data;
       }
     } catch (error) {
       console.error("Ошибка при отправке email", error);
-      // return { status: 'error', message: "Ошибка при отправке email" };
     }
   },
 
@@ -106,6 +215,30 @@ const useRegistration = create((set, get) => ({
       console.error("Ошибка при подтверждении пин-кода", error);
     }
   },
+
+  registerUser: async (password) => {
+    if (!get().emailVerified || !get().phoneVerified) {
+      console.error("Email или телефон не подтвержден.");
+      return;
+    }
+    try {
+      const response = await axios.post(`${config.backServer}/api/registration/newuser`, {
+        email: get().email,
+        phone: get().phone,
+        password,
+      }, { withCredentials: true });
+  
+      if (response.data.status === "ok") {
+        console.log("Пользователь зарегистрирован:", response.data);
+        // Можете добавить здесь дополнительные действия после регистрации, например переход на другую страницу или вывод сообщения об успешной регистрации
+      } else {
+        console.error(response.data.message);
+      }
+    } catch (error) {
+      console.error("Ошибка при регистрации пользователя", error);
+    }
+  },
+  
 }));
 
 export default useRegistration;
