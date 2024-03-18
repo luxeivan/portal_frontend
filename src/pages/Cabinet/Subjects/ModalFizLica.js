4744da3323be4405b702dd5707b6d050bcd1dafc
Stanislav <yanut995@gmail.com>
2024-03-18 14:05:15 +0300

    Added placeholder and required

--- src/pages/Cabinet/Subjects/ModalFizLica.js
+++ src/pages/Cabinet/Subjects/ModalFizLica.js
@@ -33,13 +33,18 @@ export default function ModalFizLica() {
   const [residenceAddress, setResidenceAddress] = useState("");
   const [options, setOptions] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [uploadedFilePath, setUploadedFilePath] = useState(null);
+  const [manualResidenceAddressInput, setManualResidenceAddressInput] =
+    useState(false);
 
   const onDocumentTypeChange = (value) => {
     setDocumentType(value);
   };
 
   const handleManualAddressCheckbox = (e) => {
+    setManualAddressInput(e.target.checked);
+  };
+
+  const handleManualAddressCheckboTwo = (e) => {
     setManualAddressInput(e.target.checked);
   };
 
@@ -53,26 +58,6 @@ export default function ModalFizLica() {
     email: state.email,
   }));
 
-  // const props = {
-  //   name: "file",
-  //   action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
-  //   headers: {
-  //     authorization: "authorization-text",
-  //   },
-  //   onChange(info) {
-  //     if (info.file.status !== "uploading") {
-  //       console.log(info.file, info.fileList);
-  //     }
-  //     if (info.file.status === "done") {
-  //       message.success(`${info.file.name} файл загружен успешно`);
-  //     } else if (info.file.status === "error") {
-  //       message.error(
-  //         `${info.file.name} файл не загрузился, попробуйте ешё раз.`
-  //       );
-  //     }
-  //   },
-  // };
-
   const uploadProps = {
     name: "file",
     headers: {
@@ -80,7 +65,7 @@ export default function ModalFizLica() {
     },
     customRequest({ file, onSuccess, onError }) {
       const formData = new FormData();
-      formData.append("file", file); // Так сервер ожидает файл
+      formData.append("file", file);
       const token = localStorage.getItem("jwt");
       axios
         .post(`${config.backServer}/api/cabinet/upload-file`, formData, {
@@ -88,7 +73,7 @@ export default function ModalFizLica() {
             "Content-Type": "multipart/form-data",
             Authorization: `Bearer ${token}`,
           },
-          withCredentials: true
+          withCredentials: true,
         })
         .then((response) => {
           console.log("Ответ сервера", response.data);
@@ -128,7 +113,7 @@ export default function ModalFizLica() {
   const debouncedFetchAddresses = useCallback(
     debounce(async (searchText) => {
       if (!searchText) {
-        setOptions([]); // Очищаем опции, если строка поиска пуста
+        setOptions([]);
         return;
       }
 
@@ -167,6 +152,10 @@ export default function ModalFizLica() {
     setSearchText(searchText);
   }, []);
 
+  const handleManualResidenceAddressCheckbox = (e) => {
+    setManualResidenceAddressInput(e.target.checked);
+  };
+
   const userPhone = authState.phone || registrationState.phone;
   const userEmail = authState.email || registrationState.email;
 
@@ -174,18 +163,57 @@ export default function ModalFizLica() {
     <Form {...formItemLayout}>
       <Divider orientation="center">ФИО</Divider>
 
-      <Form.Item label="Фамилия">
-        <Input />
+      <Form.Item
+        label="Фамилия"
+        name="surname"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, введите вашу фамилию",
+          },
+        ]}
+      >
+        <Input placeholder="Иванов" />
       </Form.Item>
-      <Form.Item label="Имя">
-        <Input />
+
+      <Form.Item
+        label="Имя"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, введите ваше имя",
+          },
+        ]}
+      >
+        <Input placeholder="Иван" />
       </Form.Item>
-      <Form.Item label="Отчество">
-        <Input />
+
+      <Form.Item
+        label="Отчество"
+        name="patronymic"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, введите ваше отчество",
+          },
+        ]}
+      >
+        <Input placeholder="Иванович" />
       </Form.Item>
+
       <Divider orientation="center">Подтверждающий документ</Divider>
 
-      <Form.Item label="Тип документа">
+      <Form.Item
+        label="Тип документа"
+        name="typedocuments"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, укажите тип документа",
+          },
+        ]}
+      >
         <Select defaultValue="passport" onChange={onDocumentTypeChange}>
           <Option value="passport">Паспорт гражданина РФ</Option>
           <Option value="other">Иной документ</Option>
@@ -194,17 +222,53 @@ export default function ModalFizLica() {
       {documentType === "passport" && (
         <>
           <Flex gap="middle" vertical>
-            <Form.Item label="Серия паспорта">
-              <Input maxLength={4} pattern="\d*" />
+            <Form.Item
+              label="Серия паспорта"
+              name="seriaspasport"
+              rules={[
+                {
+                  required: true,
+                  message: "Пожалуйста, укажите серию паспорта",
+                },
+              ]}
+            >
+              <Input maxLength={4} pattern="\d*" placeholder="1234" />
             </Form.Item>
-            <Form.Item label="Номер паспорта">
-              <Input maxLength={6} pattern="\d*" />
+            <Form.Item
+              label="Номер паспорта"
+              name="numberpasport"
+              rules={[
+                {
+                  required: true,
+                  message: "Пожалуйста, укажите номер паспорта",
+                },
+              ]}
+            >
+              <Input maxLength={6} pattern="\d*" placeholder="567890" />
             </Form.Item>
-            <Form.Item label="Код подразделения">
-              <Input maxLength={6} pattern="\d*" />
+            <Form.Item
+              label="Код подразделения"
+              name="kodpodrazdelenia"
+              rules={[
+                {
+                  required: true,
+                  message: "Пожалуйста, укажите код подразделения",
+                },
+              ]}
+            >
+              <Input maxLength={6} pattern="\d*" placeholder="123456" />
             </Form.Item>
-            <Form.Item label="Кем выдан">
-              <Input />
+            <Form.Item
+              label="Кем выдан"
+              name="kemvidan"
+              rules={[
+                {
+                  required: true,
+                  message: "Пожалуйста, укажите кем выдан паспорт",
+                },
+              ]}
+            >
+              <Input placeholder="..." />
             </Form.Item>
           </Flex>
         </>
@@ -217,22 +281,58 @@ export default function ModalFizLica() {
               <Option value="type2">Тип2</Option>
             </Select>
           </Form.Item>
-          <Form.Item label="Реквизиты документа">
-            <Input />
+          <Form.Item
+            label="Реквизиты документа"
+            name="recvizitydocumenta"
+            rules={[
+              {
+                required: true,
+                message: "Пожалуйста, укажите реквизиты документа",
+              },
+            ]}
+          >
+            <Input placeholder="..." />
           </Form.Item>
         </>
       )}
-      <Form.Item label="Загрузить файл">
+      <Form.Item
+        label="Загрузить файл"
+        name="uploader"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, загрузите файл",
+          },
+        ]}
+      >
         <Upload {...uploadProps}>
           <Button icon={<UploadOutlined />}>Загрузить файл</Button>
         </Upload>
       </Form.Item>
       <Divider orientation="center">СНИЛС</Divider>
-      <Form.Item label="Номер">
-        <Input />
+      <Form.Item
+        label="Номер"
+        name="snils"
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, введите номер СНИЛС",
+          },
+        ]}
+      >
+        <Input placeholder="..." />
       </Form.Item>
       <Divider orientation="center">Место регистрации</Divider>
-      <Form.Item label="Адрес" name={"registration"}>
+      <Form.Item
+        label="Адрес"
+        name={"registration"}
+        rules={[
+          {
+            required: true,
+            message: "Пожалуйста, введите адрес регистрации",
+          },
+        ]}
+      >
         {manualAddressInput ? (
           <Input />
         ) : (
@@ -261,7 +361,16 @@ export default function ModalFizLica() {
           Совпадает с адресом по месту регистрации
         </Checkbox>
       </Form.Item>
-      <Form.Item label="Адрес">
+      <Form.Item
+        label="Адрес"
+        name="livingadres"
+        rules={[
+          {
+            required: true,
+            message: "Введите город, улицу, номер дома и квартиру",
+          },
+        ]}
+      >
         {manualAddressInput ? (
           <Input />
         ) : (
@@ -269,269 +378,41 @@ export default function ModalFizLica() {
             options={options}
             onSelect={onSelect}
             onSearch={onSearch}
-            placeholder="Начните вводить"
+            placeholder="Введите город, улицу, номер дома и квартиру"
           />
         )}
       </Form.Item>
       <Form.Item>
         <Checkbox
-          checked={manualAddressInput}
-          onChange={handleManualAddressCheckbox}
+          checked={manualResidenceAddressInput}
+          onChange={handleManualResidenceAddressCheckbox}
         >
           Ввести адрес по полям вручную
         </Checkbox>
       </Form.Item>
       <Divider orientation="center">Другое</Divider>
-      <Form.Item label="Мобильный номер телефона">
-        <Input defaultValue={userPhone} onChange={handlePhoneChange} />
+      <Form.Item
+        label="Мобильный номер телефона"
+        name="phone"
+        rules={[{ required: true, message: "Введите номер телефона" }]}
+      >
+        <Input
+          defaultValue={userPhone}
+          onChange={handlePhoneChange}
+          placeholder="+7 (123) 456-78-90"
+        />
       </Form.Item>
-      <Form.Item label="Адрес электронной почты">
-        <Input defaultValue={userEmail} onChange={handleEmailChange} />
+      <Form.Item
+        label="Адрес электронной почты"
+        name="email"
+        rules={[{ required: true, message: "Введите email" }]}
+      >
+        <Input
+          defaultValue={userEmail}
+          onChange={handleEmailChange}
+          placeholder="ivanov@yandex.ru"
+        />
       </Form.Item>
     </Form>
   );
 }
-
-// import React, { useState } from "react";
-// import axios from "axios";
-// import config from "../../../config";
-
-// import {
-//   Flex,
-//   Input,
-//   Form,
-//   Select,
-//   AutoComplete,
-//   Checkbox,
-//   Button,
-//   Upload,
-//   Divider,
-//   message,
-// } from "antd";
-
-// import useAuth from "../../../stores/./useAuth";
-// import useRegistration from "../../.././stores/useRegistration";
-
-// import { UploadOutlined } from "@ant-design/icons";
-
-// import { formItemLayout } from "../../.././components/configSizeForm";
-
-// const { Option } = Select;
-
-// export default function ModalFizLica() {
-//   const [documentType, setDocumentType] = useState("passport");
-//   const [manualAddressInput, setManualAddressInput] = useState(false);
-//   const [registrationAddress, setRegistrationAddress] = useState("");
-//   const [isAddressSame, setIsAddressSame] = useState(false);
-//   const [residenceAddress, setResidenceAddress] = useState("");
-//   const [options, setOptions] = useState([]);
-
-//   const onDocumentTypeChange = (value) => {
-//     setDocumentType(value);
-//   };
-
-//   const handleManualAddressCheckbox = (e) => {
-//     setManualAddressInput(e.target.checked);
-//   };
-
-//   const authState = useAuth((state) => ({
-//     phone: state.phone,
-//     email: state.email,
-//   }));
-
-//   const registrationState = useRegistration((state) => ({
-//     phone: state.phone,
-//     email: state.email,
-//   }));
-
-//   const props = {
-//     name: "file",
-//     action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
-//     headers: {
-//       authorization: "authorization-text",
-//     },
-//     onChange(info) {
-//       if (info.file.status !== "uploading") {
-//         console.log(info.file, info.fileList);
-//       }
-//       if (info.file.status === "done") {
-//         message.success(`${info.file.name} файл загружен успешно`);
-//       } else if (info.file.status === "error") {
-//         message.error(
-//           `${info.file.name} файл не загрузился, попробуйте ешё раз.`
-//         );
-//       }
-//     },
-//   };
-
-//   const handlePhoneChange = (e) => {
-//     console.log("Новый номер телефона:", e.target.value);
-//   };
-
-//   const handleEmailChange = (e) => {
-//     console.log("Новый email:", e.target.value);
-//   };
-
-//   const handleAddressCheckboxChange = (e) => {
-//     setIsAddressSame(e.target.checked);
-//     if (e.target.checked) {
-//       setResidenceAddress(registrationAddress);
-//     }
-//   };
-
-//   const fetchAddresses = async (searchText) => {
-//     try {
-//       const token = localStorage.getItem("jwt");
-//       const response = await axios.get(
-//         `${config.backServer}/api/cabinet/get-fias`,
-//         {
-//           params: { searchString: searchText },
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${token}`,
-//           },
-//         }
-//       );
-//       console.log(response.data.data);
-//       let preparingData = response.data.data.map((item) => ({
-//         value: item.value,
-//       }));
-//       return preparingData;
-//     } catch (error) {
-//       console.error("Ошибка при получении адресов: ", error);
-//       return [];
-//     }
-//   };
-
-//   const onSelect = (data) => {
-//     console.log("onSelect", data);
-//   };
-
-//   const userPhone = authState.phone || registrationState.phone;
-//   const userEmail = authState.email || registrationState.email;
-
-//   return (
-//     <Form {...formItemLayout}>
-//       <Divider orientation="center">ФИО</Divider>
-
-//       <Form.Item label="Фамилия">
-//         <Input />
-//       </Form.Item>
-//       <Form.Item label="Имя">
-//         <Input />
-//       </Form.Item>
-//       <Form.Item label="Отчество">
-//         <Input />
-//       </Form.Item>
-//       <Divider orientation="center">Подтверждающий документ</Divider>
-
-//       <Form.Item label="Тип документа">
-//         <Select defaultValue="passport" onChange={onDocumentTypeChange}>
-//           <Option value="passport">Паспорт гражданина РФ</Option>
-//           <Option value="other">Иной документ</Option>
-//         </Select>
-//       </Form.Item>
-//       {documentType === "passport" && (
-//         <>
-//           <Flex gap="middle" vertical>
-//             <Form.Item label="Серия паспорта">
-//               <Input maxLength={4} pattern="\d*" />
-//             </Form.Item>
-//             <Form.Item label="Номер паспорта">
-//               <Input maxLength={6} pattern="\d*" />
-//             </Form.Item>
-//             <Form.Item label="Код подразделения">
-//               <Input maxLength={6} pattern="\d*" />
-//             </Form.Item>
-//             <Form.Item label="Кем выдан">
-//               <Input />
-//             </Form.Item>
-//           </Flex>
-//         </>
-//       )}
-//       {documentType === "other" && (
-//         <>
-//           <Form.Item label="Тип документа">
-//             <Select>
-//               <Option value="type1">Тип1</Option>
-//               <Option value="type2">Тип2</Option>
-//             </Select>
-//           </Form.Item>
-//           <Form.Item label="Реквизиты документа">
-//             <Input />
-//           </Form.Item>
-//         </>
-//       )}
-//       <Form.Item label="Загрузить файл">
-//         <Upload {...props}>
-//           <Button icon={<UploadOutlined />}></Button>
-//         </Upload>
-//       </Form.Item>
-//       <Divider orientation="center">СНИЛС</Divider>
-//       <Form.Item label="Номер">
-//         <Input />
-//       </Form.Item>
-//       <Divider orientation="center">Место регистрации</Divider>
-//       <Form.Item label="Адрес" name={"registration"}>
-//         {manualAddressInput ? (
-//           <Input />
-//         ) : (
-//           <AutoComplete
-//             options={options}
-//             onSelect={onSelect}
-//             onSearch={async (text) => {
-//               setOptions(await fetchAddresses(text));
-//             }}
-//             placeholder="Начните вводить"
-//           />
-//         )}
-//       </Form.Item>
-//       <Form.Item>
-//         <Checkbox
-//           checked={manualAddressInput}
-//           onChange={handleManualAddressCheckbox}
-//         >
-//           Ввести адрес по полям вручную
-//         </Checkbox>
-//       </Form.Item>
-//       <Divider orientation="center">Место проживания</Divider>
-//       <Form.Item>
-//         <Checkbox
-//           checked={isAddressSame}
-//           onChange={handleAddressCheckboxChange}
-//         >
-//           Совпадает с адресом по месту регистрации
-//         </Checkbox>
-//       </Form.Item>
-//       <Form.Item label="Адрес">
-//         {manualAddressInput ? (
-//           <Input />
-//         ) : (
-//           <AutoComplete
-//             options={options}
-//             onSelect={onSelect}
-//             onSearch={async (text) => {
-//               setOptions(await fetchAddresses(text));
-//             }}
-//             placeholder="Начните вводить"
-//           />
-//         )}
-//       </Form.Item>
-//       <Form.Item>
-//         <Checkbox
-//           checked={manualAddressInput}
-//           onChange={handleManualAddressCheckbox}
-//         >
-//           Ввести адрес по полям вручную
-//         </Checkbox>
-//       </Form.Item>
-//       <Divider orientation="center">Другое</Divider>
-//       <Form.Item label="Мобильный номер телефона">
-//         <Input defaultValue={userPhone} onChange={handlePhoneChange} />
-//       </Form.Item>
-//       <Form.Item label="Адрес электронной почты">
-//         <Input defaultValue={userEmail} onChange={handleEmailChange} />
-//       </Form.Item>
-//     </Form>
-//   );
-// }
