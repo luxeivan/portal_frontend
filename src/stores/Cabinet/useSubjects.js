ab1d64426b24ae1db6da27c8f1f15ea24e4da693
Stanislav <yanut995@gmail.com>
2024-03-19 10:47:30 +0300

    Ivan, help me with submitNewSubject

--- src/stores/Cabinet/useSubjects.js
+++ src/stores/Cabinet/useSubjects.js
@@ -10,7 +10,9 @@ const useSubjects = create((set) => ({
   error: null,
   firstName: "",
   lastName: "",
-  surname: "",
+  secondname: "",
+  snils: "",
+
 
   fetchSubjects: async () => {
     try {
@@ -56,28 +58,121 @@ const useSubjects = create((set) => ({
     }
   },
 
-  submitNewSubject: async (firstName, lastName, surname) => {
+  submitNewSubject: async (formData) => {
     try {
       const response = await axios.post(
         `${config.backServer}/api/cabinet/subjects`,
-        { firstName, lastName, surname },
-        { withCredentials: true }
+        formData, 
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+          withCredentials: true,
+        }
       );
+
       if (response.data.status === "ok") {
-        set(() => ({
-          firstName,
-          lastName,
-          surname,
-          // emailSubmitted: true,
-          // codeRequestedEmail: true,
+        set((state) => ({
+          subjects: [...state.subjects, response.data.subject],
         }));
       } else {
-        console.error(response.data.message);
+        throw new Error(response.data.message || 'Произошла ошибка при создании субъекта');
       }
     } catch (error) {
-      console.error("Ошибка при создании нового субъекта", error);
+      throw new Error(error.response?.data?.message || error.message);
     }
   },
+  
+  
+
 }));
 
 export default useSubjects;
+
+
+
+// import create from "zustand";
+// import axios from "axios";
+// import config from "../../config";
+
+// const useSubjects = create((set) => ({
+//   subjects: [],
+//   subject: null,
+//   isLoadingSubjects: false,
+//   isLoadingSubjectItem: false,
+//   error: null,
+//   firstName: "",
+//   lastName: "",
+//   secondname: "",
+//   snils: "",
+
+
+//   fetchSubjects: async () => {
+//     try {
+//       set({ isLoadingSubjects: true });
+//       const token = localStorage.getItem("jwt");
+//       const response = await axios.get(
+//         `${config.backServer}/api/cabinet/subjects`,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+//       set({ subjects: response.data, isLoadingSubjects: false });
+//     } catch (error) {
+//       console.log(error);
+//       set({
+//         error: error.response?.data?.message || error.message,
+//         isLoading: false,
+//       });
+//     }
+//   },
+//   fetchSubjectItem: async (id) => {
+//     try {
+//       set({ isLoadingSubjectItem: true });
+//       const token = localStorage.getItem("jwt");
+//       const response = await axios.get(
+//         `${config.backServer}/api/cabinet/subjects/${id}`,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+//       console.log(response.data);
+//       set({ subject: response.data, isLoadingSubjectItem: false });
+//     } catch (error) {
+//       console.log(error);
+//       set({
+//         error: error.response?.data?.message || error.message,
+//         isLoading: false,
+//       });
+//     }
+//   },
+
+  // submitNewSubject: async (firstName, lastName, secondname, snils) => {
+  //   try {
+  //     const response = await axios.post(
+  //       `${config.backServer}/api/cabinet/subjects`,
+  //       { firstName, lastName, secondname, snils },
+  //       { withCredentials: true }
+  //     );
+  //     if (response.data.status === "ok") {
+  //       set(() => ({
+  //         firstName,
+  //         lastName,
+  //         secondname,
+  //         snils,
+  //       }));
+  //     } else {
+  //       console.error(response.data.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Ошибка при создании нового субъекта", error);
+  //   }
+  // },
+// }));
+
+// export default useSubjects;
