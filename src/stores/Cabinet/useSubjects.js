c5e41307b66c430e3a518c5b5eeb301f6c6b22f3
Stanislav <yanut995@gmail.com>
2024-03-25 11:32:18 +0300

    Added mask number

--- src/stores/Cabinet/useSubjects.js
+++ src/stores/Cabinet/useSubjects.js
@@ -95,7 +95,7 @@ const useSubjects = create((set, get) => ({
   // Действие для установки адресных опций
   setAddressOptions: (options) => set({ addressOptions: options }),
 
-  // Задержанная функция для получения адресов
+  // Делаем хадержку запроса для получения адресов
   debouncedFetchAddresses: debounce(async (searchText) => {
     if (!searchText) {
       get().setAddressOptions([]);
@@ -130,11 +130,45 @@ const useSubjects = create((set, get) => ({
       get().setAddressOptions([]);
     }
   }, 800),
+
+  // Валидируем СНИЛС
+  validateSnils: (snils) => {
+    const error = { code: 0, message: "" };
+    if (typeof snils !== "string") snils = "";
+    if (!snils.length) {
+      error.code = 1;
+      error.message = "СНИЛС пуст";
+    } else if (snils.length !== 14) {
+      error.code = 2;
+      error.message = "СНИЛС должен состоять из 11 цифр";
+    } else {
+      const nums = snils.replace(/[^0-9]/g, "");
+      if (nums.length !== 11) {
+        error.code = 3;
+        error.message = "Неправильный формат СНИЛС";
+      } else {
+        let sum = 0;
+        for (let i = 0; i < 9; i++) {
+          sum += parseInt(nums[i]) * (9 - i);
+        }
+        let checkDigit = sum < 100 ? sum : sum % 101;
+        if (checkDigit === 100) checkDigit = 0;
+        if (checkDigit === parseInt(nums.slice(-2))) {
+          return Promise.resolve();
+        } else {
+          error.code = 4;
+          error.message = "Неверный контрольный номер";
+        }
+      }
+    }
+    return Promise.reject(new Error(error.message));
+  },
+
 }));
 
-export default useSubjects;
 
-// import {create} from "zustand";
+
+export default useSubjects;
 // import axios from "axios";
 // import config from "../../config";
 
