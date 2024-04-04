86f7934260bcaff47df9590f3998331917f12976
luxeivan <luxeivan@gmail.com>
2024-04-04 16:14:27 +0300

    change services

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,11 +1,129 @@
 import React, { useEffect } from "react";
 import AppHelmet from "../../components/Global/AppHelmet";
-import { Card, Flex, Typography, Image } from "antd";
+import { Card, Flex, Typography, Image, Divider } from "antd";
 import { Link, useParams } from "react-router-dom";
 import useServices from "../../stores/useServices";
 import styles from "./Services.module.css";
 import config from "../../config";
 const { Title, Text } = Typography;
+
+
+const serviceDetailsData = [
+  {
+    url: "uslugi-tehnologicheskogo-prisoedineniyas",
+    title: "Услуги технологического присоединения",
+    content: "Подключение энергопринимающих устройств к электрическим сетям",
+    subServices: [
+      {
+        title: "Для физических лиц",
+        name: "Физические лица",
+        url: "fiz-lica"
+      },
+      {
+        title: "Для юридических лиц",
+        name: "Юридические лица",
+        url: "yur-lica"
+      },
+      {
+        title: "Для индивидуальных предпринимателей",
+        name: "Индивидуальные предприниматели",
+        url: "individual"
+      },
+      {
+        title: "Для энергосбытовых организаций",
+        name: "Энергосбытовая организация",
+        url: "energo-org"
+      },
+    ],
+  },
+  {
+    url: "kommercheskie-uslugis",
+    title: "Коммерческие услуги",
+    content: "Аренда, Строительство, Ремонт",
+    subServices: [
+      {
+        title: "Ремонт, техническое и оперативное обслуживание",
+        name: "Ремонт, техническое и оперативное обслуживание",
+        url: "remont"
+      },
+      {
+        title: "Услуги аренды",
+        name: "Услуги аренды",
+        url: "uslugi"
+      },
+      {
+        title: "Обслуживание приборов учёта",
+        name: "Обслуживание приборов учёта",
+        url: "obslujivanie"
+      },
+      {
+        title: "Дополнительные услуги в рамках технологического присоединения",
+        name: "Дополнительные услуги в рамках технологического присоединения",
+        url: "dopuslugi"
+      },
+      {
+        title: "Освобождение земельного участка от электрических сетей",
+        name: "Освобождение земельного участка от электрических сетей",
+        url: "osvobojdenie"
+      },
+    ],
+  },
+  {
+    url: "uchet-elektricheskoj-energiis",
+    title: "Учёт электрической энергии",
+    content: "Установка, замена и поверка приборов учета",
+    subServices: [
+      {
+        title: "Для физических лиц",
+        name: "Физические лица",
+        url: "fiz-lica"
+      },
+      {
+        title: "Для юридических лиц",
+        name: "Юридические лица",
+        url: "yur-lica"
+      },
+      {
+        title: "Для индивидуальных предпринимателей",
+        name: "Индивидуальные предприниматели",
+        url: "individual"
+      },
+      {
+        title: "Сетевые организации",
+        name: "Сетевые организации",
+        url: "setevie"
+      },
+      {
+        title: "Для энергосбытовых организаций",
+        name: "Энергосбытовая организация",
+        url: "energo-org"
+      },
+    ],
+  },
+  {
+    url: "servisnye-uslugis",
+    title: "Сервисные услуги",
+    content: "Техническая поддержка пользователей",
+    subServices: [
+      {
+        title: "Актуальная информация профиля",
+        name: "Актуальная информация профиля",
+        url: "info"
+      },
+      {
+        title: "Подписка на информационные сообщения",
+        name: "Подписка на информационные сообщения",
+        url: "subscribe"
+      },
+      {
+        title: "Прочее",
+        name: "Прочее",
+        url: "other"
+      },
+    ],
+  },
+];
+
 
 export default function Services() {
   const services = useServices((state) => state.services);
@@ -13,7 +131,7 @@ export default function Services() {
   const { level2, level3 } = useParams();
   useEffect(() => {
     if (level2 && level3) {
-      fetchServices(level2, level3);
+      fetchServices(level2, serviceDetailsData.find(item => item.url === level2).subServices.find(item => item.url === level3).name);
     }
   }, [level2, level3]);
   //console.log(services)
@@ -54,7 +172,7 @@ export default function Services() {
                   <Link
                     key={index}
                     to={`/services/${serviceDetailsData.find((item) => item.url === level2).url
-                      }/${item.title}`}
+                      }/${item.url}`}
                     className={styles.styleLink}
                   >
                     <Card className={styles.styleCard}>
@@ -69,9 +187,9 @@ export default function Services() {
         {level2 && level3 && (
           <>
             <Title level={1} className={styles.title}>
-              {serviceDetailsData.find((item) => item.url === level2).title} -{" "}
-              {level3}
+            {serviceDetailsData.find((item) => item.url === level2).title}: <span style={{textTransform:"lowercase"}}>{serviceDetailsData.find(item => item.url === level2).subServices.find(item => item.url === level3).title}</span>
             </Title>
+            
             <Flex wrap="wrap" gap="large">
               {services &&
                 services.map((item) => (
@@ -110,52 +228,3 @@ export default function Services() {
   );
 }
 
-const serviceDetailsData = [
-  {
-    url: "uslugi-tehnologicheskogo-prisoedineniyas",
-    title: "Услуги технологического присоединения",
-    content: "Подключение энергопринимающих устройств к электрическим сетям",
-    subServices: [
-      { title: "Физические лица" },
-      { title: "Юридические лица" },
-      { title: "Индивидуальные предприниматели" },
-      { title: "Энергосбытовая организация" },
-    ],
-  },
-  {
-    url: "kommercheskie-uslugis",
-    title: "Коммерческие услуги",
-    content: "Аренда, Строительство, Ремонт",
-    subServices: [
-      { title: "Ремонт, техническое и оперативное обслуживание" },
-      { title: "Услуги аренды" },
-      { title: "Обслуживание приборов учёта" },
-      {
-        title: "Дополнительные услуги в рамках технологического присоединения",
-      },
-      { title: "Освобождение земельного участка от электрических сетей" },
-    ],
-  },
-  {
-    url: "uchet-elektricheskoj-energiis",
-    title: "Учёт электрической энергии",
-    content: "Установка, замена и поверка приборов учета",
-    subServices: [
-      { title: "Физические лица" },
-      { title: "Юридические лица" },
-      { title: "Индивидуальные предприниматели" },
-      { title: "Сетевые организации" },
-      { title: "Энергосбытовая организация" },
-    ],
-  },
-  {
-    url: "servisnye-uslugis",
-    title: "Сервисные услуги",
-    content: "Техническая поддержка пользователей",
-    subServices: [
-      { title: "Актуальная информация профиля" },
-      { title: "Подписка на информационные сообщения" },
-      { title: "Прочее" },
-    ],
-  },
-];
