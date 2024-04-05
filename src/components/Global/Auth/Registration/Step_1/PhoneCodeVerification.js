4757cf7055fe7140a00f0d320961bb02c9cae9ac
Stanislav <yanut995@gmail.com>
2024-04-05 11:28:39 +0300

    Update EmailCodeVerification

--- src/components/Global/Auth/Registration/Step_1/PhoneCodeVerification.js
+++ src/components/Global/Auth/Registration/Step_1/PhoneCodeVerification.js
@@ -78,9 +78,7 @@ const PhoneCodeVerification = () => {
           onChange={handleInput4Change}
         />
       </Form.Item>
-      {/* <Button type="primary" htmlType="submit" className={styles.submitButton}>
-        Продолжить
-      </Button> */}
+
     </Form>
   );
 };
--- src/components/Global/Auth/Registration/Step_2/EmailCodeVerification.js
+++ src/components/Global/Auth/Registration/Step_2/EmailCodeVerification.js
@@ -1,29 +1,76 @@
-import React from "react";
-import { Form, Input, Button } from "antd";
+import React, { useRef } from "react";
+import { Form, Input } from "antd";
 import useRegistration from "../../../../../stores/useRegistration";
+import styles from "./EmailCodeVerification.module.css"; // Убедитесь, что путь к CSS-файлу правильный
 
 const EmailCodeVerification = () => {
   const submitEmailCode = useRegistration((state) => state.submitEmailCode);
+  const formRef = useRef(null);
+  const inputRefs = useRef([]);
 
   const onFinish = (values) => {
-    submitEmailCode(values.emailCode);
+    const emailCode = Object.values(values).join("");
+    submitEmailCode(emailCode);
+  };
+
+  const handleInputChange = (e, index) => {
+    if (e.target.value.length === 1) {
+      if (index < 3) {
+        inputRefs.current[index + 1].focus();
+      } else {
+        formRef.current.submit();
+      }
+    }
   };
 
   return (
-    <Form onFinish={onFinish}>
-      <Form.Item
-        name="emailCode"
-        rules={[{ required: true, message: "Пожалуйста, введите пин-код!" }]}
-      >
-        <Input placeholder="Пин-код" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Подтвердить
-        </Button>
-      </Form.Item>
+    <Form ref={formRef} onFinish={onFinish} className={styles.codeFormContainer}>
+      {Array.from({ length: 4 }, (_, index) => (
+        <Form.Item
+          key={index}
+          name={`pin${index + 1}`}
+          rules={[{ required: true, message: "Введите цифру" }]}
+        >
+          <Input
+            className={styles.codeInput}
+            ref={(el) => (inputRefs.current[index] = el)}
+            maxLength={1}
+            onChange={(e) => handleInputChange(e, index)}
+          />
+        </Form.Item>
+      ))}
     </Form>
   );
 };
 
 export default EmailCodeVerification;
+
+// import React from "react";
+// import { Form, Input, Button } from "antd";
+// import useRegistration from "../../../../../stores/useRegistration";
+
+// const EmailCodeVerification = () => {
+//   const submitEmailCode = useRegistration((state) => state.submitEmailCode);
+
+//   const onFinish = (values) => {
+//     submitEmailCode(values.emailCode);
+//   };
+
+//   return (
+//     <Form onFinish={onFinish}>
+//       <Form.Item
+//         name="emailCode"
+//         rules={[{ required: true, message: "Пожалуйста, введите пин-код!" }]}
+//       >
+//         <Input placeholder="Пин-код" />
+//       </Form.Item>
+//       <Form.Item>
+//         <Button type="primary" htmlType="submit">
+//           Подтвердить
+//         </Button>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
+// export default EmailCodeVerification;
