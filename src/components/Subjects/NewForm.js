7171a3ef3188344c1a843bda441e1ff6ab1c0899
Stanislav <yanut995@gmail.com>
2024-04-16 14:15:54 +0300

    Что-то работает, а что-то нет

--- src/components/Subjects/NewForm.js
+++ src/components/Subjects/NewForm.js
@@ -1,162 +1,284 @@
 import React from "react";
-import { Divider, Form, Button, Input, DatePicker } from "antd";
+import { Divider, Form, Button } from "antd";
 import TextInput from "../FormComponents/TextInput";
 import SelectInput from "../FormComponents/SelectInput";
 import AddressInput from "../FormComponents/AddressInput";
 import SnilsInput from "../FormComponents/SnilsInput";
-import UploaderInput from "../FormComponents/UploaderInput";
+import ConfirmationDocumentInput from "../FormComponents/ConfirmationDocumentInput";
 import { formItemLayout } from "../../components/configSizeForm";
-import moment from "moment";
-import "moment/locale/ru";
 
-moment.locale("ru");
-
-const renderCustomInput = (field, value) => {
-  switch (field.inputType) {
-    case "textarea":
-      return <Input.TextArea {...field} value={value} />;
-    case "datepicker":
-      return (
-        <DatePicker
-          {...field}
-          value={value ? moment(value, field.format) : null}
-        />
-      );
-    // ... другие кастомные типы ввода можно потом добавить здесь
-    default:
-      return null;
-  }
-};
-
-const renderField = (
-  field,
-  index,
-  form,
-  value,
-  read,
-  edit,
-  setShowModal,
-  handlerDelete
-) => {
-  switch (field.type) {
-    case "customInput":
-      return (
-        <Form.Item key={index} label={field.displayName} name={field.name}>
-          {renderCustomInput(field, value[field.name])}
-        </Form.Item>
-      );
-      
-    case "divider":
-      return <Divider key={index}>{field.name}</Divider>;
-
-    case "textInput":
-      return (
-        <TextInput
-          key={index}
-          {...field}
-          value={value[field.name]}
-          read={read}
-          edit={edit}
-        />
-      );
-    case "selectInput":
-      return (
-        <SelectInput
-          key={index}
-          {...field}
-          value={value[field.name]}
-          read={read}
-          edit={edit}
-        />
-      );
-    case "addressInput":
-      return (
-        <AddressInput
-          key={index}
-          {...field}
-          value={value[field.name]}
-          form={form}
-          read={read}
-          edit={edit}
-        />
-      );
-    case "snilsInput":
-      return (
-        <SnilsInput
-          key={index}
-          {...field}
-          value={value[field.name]}
-          read={read}
-          edit={edit}
-        />
-      );
-    case "uploaderInput":
-      return (
-        <UploaderInput
-          key={index}
-          {...field}
-          value={value[field.name]}
-          read={read}
-          edit={edit}
-        />
-      );
-    default:
-      return null;
-  }
-};
-
-export default function NewForm({
-  handlerDelete,
+const NewForm = ({
   fields,
   read = false,
   edit = false,
-  value = false,
+  value = {},
   setShowModal,
-}) {
+}) => {
   const [form] = Form.useForm();
 
+  const renderField = (field, index) => {
+    switch (field.type) {
+      case "divider":
+        return <Divider key={index}>{field.name}</Divider>;
+      case "textInput":
+        return (
+          <TextInput
+            key={index}
+            {...field}
+            read={read}
+            edit={edit}
+            value={value[field.name]}
+          />
+        );
+      case "selectInput":
+        return (
+          <SelectInput
+            key={index}
+            {...field}
+            read={read}
+            edit={edit}
+            value={value[field.name]}
+          />
+        );
+      case "addressInput":
+        return (
+          <AddressInput
+            key={index}
+            {...field}
+            form={form}
+            read={read}
+            edit={edit}
+            value={value[field.name]}
+          />
+        );
+      case "snilsInput":
+        return (
+          <SnilsInput
+            key={index}
+            {...field}
+            read={read}
+            edit={edit}
+            value={value[field.name]}
+          />
+        );
+      case "confirmationDocumentInput":
+        return (
+          <ConfirmationDocumentInput
+            key={index}
+            form={form}
+            {...field}
+            read={read}
+            edit={edit}
+            value={value}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
-    <Form
-      form={form}
-      {...formItemLayout}
-      onFinish={(values) => console.log(values)}
-    >
-      {fields?.length > 0 &&
-        fields.map((field, index) =>
-          renderField(
-            field,
-            index,
-            form,
-            value,
-            read,
-            edit,
-            setShowModal,
-            handlerDelete
-          )
+    <>
+      <Form form={form} {...formItemLayout}>
+        {fields?.map((field, index) => renderField(field, index))}
+        {!read && (
+          <Form.Item>
+            <Button type="primary" onClick={() => form.submit()}>
+              Добавить
+            </Button>{" "}
+            <Button onClick={() => setShowModal(false)}>Закрыть</Button>
+          </Form.Item>
         )}
-      {!read && (
-        <Form.Item>
-          <Button type="primary" onClick={() => form.submit()}>
-            Добавить
-          </Button>
-          <Button onClick={() => setShowModal(false)}>Закрыть</Button>
-        </Form.Item>
-      )}
+      </Form>
       {read && (
         <Form.Item>
           <Button type="primary" onClick={() => console.log("Изменить")}>
             Изменить
-          </Button>
-          <Button type="primary" danger onClick={() => handlerDelete(value.id)}>
+          </Button>{" "}
+          <Button type="primary" danger onClick={() => console.log("Удалить")}>
             Удалить
-          </Button>
+          </Button>{" "}
           <Button onClick={() => setShowModal(false)}>Закрыть</Button>
         </Form.Item>
       )}
-    </Form>
+    </>
   );
-}
+};
 
+export default NewForm;
+
+// import React from "react";
+// import { Divider, Form, Button, Input, DatePicker } from "antd";
+// import TextInput from "../FormComponents/TextInput";
+// import SelectInput from "../FormComponents/SelectInput";
+// import AddressInput from "../FormComponents/AddressInput";
+// import SnilsInput from "../FormComponents/SnilsInput";
+// import UploaderInput from "../FormComponents/UploaderInput";
+// import { formItemLayout } from "../../components/configSizeForm";
+// import moment from "moment";
+// import "moment/locale/ru";
+// import ConfirmationDocumentInput from "../FormComponents/ConfirmationDocumentInput";
+
+// moment.locale("ru");
+
+// const renderCustomInput = (field, value) => {
+//   switch (field.inputType) {
+//     case "textarea":
+//       return <Input.TextArea {...field} value={value} />;
+//     case "datepicker":
+//       return (
+//         <DatePicker
+//           {...field}
+//           value={value ? moment(value, field.format) : null}
+//         />
+//       );
+//     // ... другие кастомные типы ввода можно потом добавить здесь
+//     default:
+//       return null;
+//   }
+// };
+
+// const renderField = (
+//   field,
+//   index,
+//   form,
+//   value,
+//   read,
+//   edit,
+//   setShowModal,
+//   handlerDelete
+// ) => {
+//   switch (field.type) {
+//     case "customInput":
+//       return (
+//         <Form.Item key={index} label={field.displayName} name={field.name}>
+//           {renderCustomInput(field, value[field.name])}
+//         </Form.Item>
+//       );
+
+//     case "divider":
+//       return <Divider key={index}>{field.name}</Divider>;
+
+//     case "textInput":
+//       return (
+//         <TextInput
+//           key={index}
+//           {...field}
+//           value={value[field.name]}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+//     case "selectInput":
+//       return (
+//         <SelectInput
+//           key={index}
+//           {...field}
+//           value={value[field.name]}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+
+//     case "confirmationDocumentInput":
+//       return (
+//         <ConfirmationDocumentInput
+//           key={index}
+//           field={field}
+//           value={value}
+//           form={form}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+
+//     case "addressInput":
+//       return (
+//         <AddressInput
+//           key={index}
+//           {...field}
+//           value={value[field.name]}
+//           form={form}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+//     case "snilsInput":
+//       return (
+//         <SnilsInput
+//           key={index}
+//           {...field}
+//           value={value[field.name]}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+//     case "uploaderInput":
+//       return (
+//         <UploaderInput
+//           key={index}
+//           {...field}
+//           value={value[field.name]}
+//           read={read}
+//           edit={edit}
+//         />
+//       );
+//     default:
+//       return null;
+//   }
+// };
+
+// export default function NewForm({
+//   handlerDelete,
+//   fields,
+//   read = false,
+//   edit = false,
+//   value = false,
+//   setShowModal,
+// }) {
+//   const [form] = Form.useForm();
+
+//   return (
+//     <Form
+//       form={form}
+//       {...formItemLayout}
+//       onFinish={(values) => console.log(values)}
+//     >
+//       {fields?.length > 0 &&
+//         fields.map((field, index) =>
+//           renderField(
+//             field,
+//             index,
+//             form,
+//             value,
+//             read,
+//             edit,
+//             setShowModal,
+//             handlerDelete
+//           )
+//         )}
+//       {!read && (
+//         <Form.Item>
+//           <Button type="primary" onClick={() => form.submit()}>
+//             Добавить
+//           </Button>
+//           <Button onClick={() => setShowModal(false)}>Закрыть</Button>
+//         </Form.Item>
+//       )}
+//       {read && (
+//         <Form.Item>
+//           <Button type="primary" onClick={() => console.log("Изменить")}>
+//             Изменить
+//           </Button>
+//           <Button type="primary" danger onClick={() => handlerDelete(value.id)}>
+//             Удалить
+//           </Button>
+//           <Button onClick={() => setShowModal(false)}>Закрыть</Button>
+//         </Form.Item>
+//       )}
+//     </Form>
+//   );
+// }
 
 // import React from "react";
 // import { Divider, Form, Button } from "antd";
