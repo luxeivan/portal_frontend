4aa0bc15530cae71f3a75e59f3a371ace96f4c6a
Stanislav <yanut995@gmail.com>
2024-04-17 10:06:40 +0300

    Добавил открытие дефолтной модалки для юр.лица

--- src/pages/Cabinet/Subjects/Subjects.js
+++ src/pages/Cabinet/Subjects/Subjects.js
@@ -5,7 +5,8 @@ import styles from "./Subjects.module.css";
 import { PlusOutlined } from "@ant-design/icons";
 import SceletonCard from "../../../components/SceletonCard";
 import ModalFizLica from "../../../components/Subjects/ModalFizLica/ModalFizLica";
-import AppHelmet from '../../../components/Global/AppHelmet'
+import AppHelmet from "../../../components/Global/AppHelmet";
+import ModalUrLica from "../../../components/Subjects/ModalUrLica/ModalUrLica";
 
 const { Title } = Typography;
 const { Meta } = Card;
@@ -23,6 +24,8 @@ const stylesForCard = {
 export default function Subjects() {
   const [showModalAdd, setShowModalAdd] = useState(false);
   const [showModalView, setShowModalView] = useState(false);
+  const [showModalUrAdd, setShowModalUrAdd] = useState(false);
+  const [showModalUrView, setShowModalUrView] = useState(false);
   const [showCategoryModal, setShowCategoryModal] = useState(false);
   const [selectedType, setSelectedType] = useState(null);
 
@@ -57,8 +60,16 @@ export default function Subjects() {
     setShowModalAdd(false);
   };
 
+  const handleCancelModalUrAdd = () => {
+    setShowModalUrAdd(false);
+  };
+
   const handleOkModalAdd = () => {
     setShowModalAdd(false);
+  };
+
+  const handleOkModalUrAdd = () => {
+    setShowModalUrAdd(false);
   };
 
   const handleCancelModalView = () => {
@@ -78,7 +89,8 @@ export default function Subjects() {
     setSelectedType(type); // Сохраняем выбранный тип
     if (type === "individual") {
       setShowModalAdd(true);
-    } else {
+    } else if (type === "legalEntity") {
+      setShowModalUrAdd(true);
       // Для других типов (ИП, Юр.Лицо) может быть своя логика
     }
   };
@@ -136,18 +148,31 @@ export default function Subjects() {
         name="type"
       >
         <Flex gap="large" wrap="wrap" justify="center">
-          <Button className={styles.buttontypenewsubject} onClick={() => handleCategorySelect("individual")}>
-            Физическое<br /> лицо
+          <Button
+            className={styles.buttontypenewsubject}
+            onClick={() => handleCategorySelect("individual")}
+          >
+            Физическое
+            <br /> лицо
           </Button>
-          <Button className={styles.buttontypenewsubject} onClick={() => handleCategorySelect("legalEntity")}>
-            Юридическое<br /> лицо
+          <Button
+            className={styles.buttontypenewsubject}
+            onClick={() => handleCategorySelect("legalEntity")}
+          >
+            Юридическое
+            <br /> лицо
           </Button>
-          <Button className={styles.buttontypenewsubject} onClick={() => handleCategorySelect("soleProprietor")}>
-            Индивидуальный<br /> предприниматель
+          <Button
+            className={styles.buttontypenewsubject}
+            onClick={() => handleCategorySelect("soleProprietor")}
+          >
+            Индивидуальный
+            <br /> предприниматель
           </Button>
         </Flex>
       </Modal>
 
+      {/* Модалка для добавления физ.лица */}
       <Modal
         title="Добавление физического лица"
         open={showModalAdd}
@@ -161,7 +186,7 @@ export default function Subjects() {
           type={selectedType}
         />
       </Modal>
-      
+      {/* Модалка для просмотра физ.лица */}
       <Modal
         title="Просмотр физического лица"
         open={showModalView}
@@ -172,14 +197,28 @@ export default function Subjects() {
         <ModalFizLica
           read
           value={{
-           ...subject
+            ...subject,
           }}
           onSubmit={handleOkModalAdd}
           setShowModal={setShowModalView}
           type={selectedType}
         />
       </Modal>
+
+      {/* Модалка для добавления юр.лица */}
+      <Modal
+        title="Добавление юридического лица"
+        open={showModalUrAdd}
+        onCancel={handleCancelModalUrAdd}
+        width={650}
+        footer={null}
+      >
+        <ModalUrLica
+          onSubmit={handleOkModalUrAdd}
+          setShowModal={setShowModalUrAdd}
+          type={selectedType}
+        />
+      </Modal>
     </div>
   );
 }
-
