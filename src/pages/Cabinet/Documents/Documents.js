b99d88263b4f1a1388af7099c0b966fbd1c43a4e
Stanislav <yanut995@gmail.com>
2024-06-06 14:30:43 +0300

    Пусть Ваня услышит, пусть Ваня придет

--- src/pages/Cabinet/Documents/Documents.js
+++ src/pages/Cabinet/Documents/Documents.js
@@ -86,11 +86,48 @@ const Documents = () => {
       });
   }
 
+  // const handleSaveDocument = async (values) => {
+  //   console.log("values", values);
+  //   console.log("fileList", fileList);
+  //   message.success("Документ успешно сохранен");
+  //   setShowModalAdd(false);
+  // };
+
   const handleSaveDocument = async (values) => {
-    console.log("values", values);
-    console.log("fileList", fileList);
-    message.success("Документ успешно сохранен");
-    setShowModalAdd(false);
+    try {
+      const formData = {
+        documentName: values.documentName,
+        files: fileList.map((file) => ({
+          name: file.name,
+          originFileObj: file.originFileObj,
+        })),
+      };
+
+      console.log(formData);
+
+      const token = localStorage.getItem("jwt");
+
+      const response = await axios.post(
+        `${config.backServer}/api/cabinet/documents`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
+        }
+      );
+
+      console.log("response", response);
+      message.success("Документ успешно сохранен");
+      setShowModalAdd(false);
+      form.resetFields();
+      setFileList([]);
+    } catch (error) {
+      console.error("Ошибка при сохранении документа:", error);
+      message.error("Не удалось сохранить документ");
+    }
   };
 
   const handleModalClose = () => {
@@ -177,18 +214,23 @@ const Documents = () => {
 
 export default Documents;
 
-// import React, { useEffect, useState } from "react";
-// import { Typography, Card, Flex, Modal, Button, Image, Upload, message, Input, Form } from "antd";
-// import useRelations from "../../../stores/Cabinet/useRelations";
-// import styles from "./Documents.module.css";
+// import React, { useState } from "react";
+// import {
+//   Typography,
+//   Card,
+//   Modal,
+//   Button,
+//   Upload,
+//   message,
+//   Form,
+//   Select,
+// } from "antd";
 // import { PlusOutlined, UploadOutlined } from "@ant-design/icons";
-// import SceletonCard from "../../../components/SceletonCard";
-// import AppHelmet from "../../../components/Global/AppHelmet";
-// import config from "../../../config";
 // import axios from "axios";
+// import config from "../../../config";
 
 // const { Title } = Typography;
-// const { Meta } = Card;
+// const { Option } = Select;
 
 // const stylesForCard = {
 //   body: {
@@ -198,30 +240,13 @@ export default Documents;
 //   },
 //   actions: { marginTop: "-20px" },
 //   header: { backgroundColor: "red" },
-// }
+// };
 
-// export default function Documents() {
-//   const [showModalAdd, setShowModalAdd] = useState(false)
-//   const [fileList, setFileList] = useState([
-//     {
-//       uid: '0',
-//       name: 'xxx.png',
-//       status: 'uploading',
-//       percent: 33,
-//     },
-//     {
-//       uid: '-1',
-//       name: 'yyy.png',
-//       status: 'done',
-//       url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-//       thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-//     },
-//     {
-//       uid: '-2',
-//       name: 'zzz.png',
-//       status: 'error',
-//     },
-//   ])
+// const Documents = () => {
+//   const [showModalAdd, setShowModalAdd] = useState(false);
+//   const [fileList, setFileList] = useState([]);
+//   const [form] = Form.useForm();
+
 //   let files = [];
 //   const getFile = async (relativePath) => {
 //     const fileblob = await axios.get(
@@ -237,7 +262,8 @@ export default Documents;
 //     if (!fileblob.data) throw new Error("Ошибка получения файла");
 
 //     return window.URL.createObjectURL(fileblob.data);
-//   }
+//   };
+
 //   function customRequest({ file, onSuccess, onError }) {
 //     const formData = new FormData();
 //     formData.append("file", file);
@@ -254,7 +280,7 @@ export default Documents;
 //       .then(async (response) => {
 //         const relativePath = response.data.files[0];
 //         files.push(relativePath);
-//         const fileUrl = await getFile(relativePath)
+//         const fileUrl = await getFile(relativePath);
 //         setFileList((prev) => [
 //           ...prev,
 //           {
@@ -275,71 +301,94 @@ export default Documents;
 //         message.error(`${file.name} файл не загрузился, попробуйте ещё раз.`);
 //       });
 //   }
+
+//   const handleSaveDocument = async (values) => {
+//     console.log("values", values);
+//     console.log("fileList", fileList);
+//     message.success("Документ успешно сохранен");
+//     setShowModalAdd(false);
+//   };
+
+//   const handleModalClose = () => {
+//     setShowModalAdd(false);
+//     form.resetFields();
+//   };
+
 //   return (
 //     <div>
-//       <AppHelmet title={"Документы"} desc={"Документы"} />
 //       <Title level={1}>Документы</Title>
-//       <Flex wrap="wrap" gap="large">
-
-//         <Card
-//         hoverable
-//           styles={stylesForCard}
-//         >
-
-//         </Card>
+//       <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+//         {fileList.map((file) => (
+//           <Card
+//             key={file.uid}
+//             hoverable
+//             style={{ width: 250, height: 250 }}
+//             cover={<img alt={file.name} src={file.url} />}
+//           >
+//             <Card.Meta title={file.name} />
+//           </Card>
+//         ))}
 //         <Card
 //           hoverable
-//           styles={stylesForCard}
-//           className={styles.objectCard}
+//           style={{
+//             width: 250,
+//             height: 250,
+//             display: "flex",
+//             alignItems: "center",
+//             justifyContent: "center",
+//           }}
 //           onClick={() => setShowModalAdd(true)}
 //         >
-//           <Flex
-//             align="stretch"
-//             justify="center"
-//             style={{ minHeight: "100%", width: "100%" }}
-//           >
-//             <PlusOutlined />
-//           </Flex>
+//           <PlusOutlined style={{ fontSize: "24px" }} />
 //         </Card>
-//       </Flex>
+//       </div>
 //       <Modal
 //         title="Загрузить новый документ"
-//         open={showModalAdd}
-//         onCancel={() => setShowModalAdd(false)}
+//         visible={showModalAdd}
+//         onCancel={handleModalClose}
 //         footer={null}
-//         width={700}
-//         name="type"
 //       >
-//         <Flex gap="large" wrap="wrap" justify="center">
-//           <Form>
-//             <Form.Item
-//               label={'Наименование документа'}
-//               name={'name'}
-//             >
-//               <Input />
-//             </Form.Item>
-//             <Form.Item
-
-//               name={'fileName'}
-//               hidden
-//             >
-//               <Input />
-//             </Form.Item>
-
+//         <Form form={form} onFinish={handleSaveDocument}>
+//           <Form.Item
+//             label="Название"
+//             name="documentName"
+//             rules={[
+//               {
+//                 required: true,
+//                 message: "Пожалуйста, выберите название документа",
+//               },
+//             ]}
+//           >
+//             <Select>
+//               <Option value="passport">Паспорт</Option>
+//               <Option value="authorization">Доверенность</Option>
+//             </Select>
+//           </Form.Item>
+//           <Form.Item
+//             label="Загрузить файл"
+//             name="files"
+//             valuePropName="fileList"
+//             getValueFromEvent={(e) => (Array.isArray(e) ? e : e && [e.file])}
+//             rules={[{ required: true, message: "Пожалуйста, загрузите файлы" }]}
+//           >
 //             <Upload
 //               listType="picture"
-//               defaultFileList={[...fileList]}
-//               className={styles.uploadListInline}
 //               customRequest={customRequest}
-//               maxCount={1}
+//               onChange={({ fileList: newFileList }) => setFileList(newFileList)}
+//               multiple
 //             >
-
-//               <Button icon={<UploadOutlined />}>Загрузить...</Button>
+//               <Button icon={<UploadOutlined />}>Загрузить</Button>
 //             </Upload>
-//             <Button icon={<UploadOutlined />}>Добавить документ(ы)</Button>
-//           </Form>
-//         </Flex>
+//           </Form.Item>
+//           <Form.Item>
+//             <Button type="primary" htmlType="submit">
+//               Сохранить файлы
+//             </Button>
+//           </Form.Item>
+//         </Form>
 //       </Modal>
 //     </div>
-//   )
-// }
+//   );
+// };
+
+// export default Documents;
