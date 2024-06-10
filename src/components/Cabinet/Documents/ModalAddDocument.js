3f99a5e2ea72d01ef008384b449071c7ec99e995
Stanislav <yanut995@gmail.com>
2024-06-10 11:25:24 +0300

    Прелоадер добавлен

--- src/components/Cabinet/Documents/ModalAddDocument.js
+++ src/components/Cabinet/Documents/ModalAddDocument.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-import {
-  Modal,
-  Button,
-  Upload,
-  message,
-  Form,
-  Select,
-  Spin,
-  Progress,
-} from "antd";
+import { Modal, Button, Form, Select, Spin, Progress, message } from "antd";
 import config from "../../../config";
 import axios from "axios";
 import useDocuments from "../../../stores/Cabinet/useDocuments";
-import { UploadOutlined } from "@ant-design/icons";
 import UploaderInput from "../../FormComponents/UploaderInput";
 const { Option } = Select;
 
@@ -21,21 +11,21 @@ export default function ModalAddDocument() {
   const setOpenModalAdd = useDocuments((state) => state.setOpenModalAdd);
   const fetchDocuments = useDocuments((state) => state.fetchDocuments);
   const nameDocs = useDocuments((state) => state.nameDocs);
-  const [fileList, setFileList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingMessage, setLoadingMessage] = useState("");
   const [uploadPercent, setUploadPercent] = useState(0);
   const [savePercent, setSavePercent] = useState(0);
 
   const [form] = Form.useForm();
+
   const handleModalClose = () => {
     setOpenModalAdd(false);
     form.resetFields();
-    setFileList([]);
+    setUploadPercent(0);
+    setSavePercent(0);
   };
 
   const handleSaveDocument = async (values) => {
-    console.log(form.getFieldValue("fileDoc"));
     try {
       setLoading(true);
       setLoadingMessage("Пожалуйста, подождите, файл сохраняется");
@@ -82,7 +72,6 @@ export default function ModalAddDocument() {
 
       setOpenModalAdd(false);
       form.resetFields();
-      setFileList([]);
       setLoading(false);
       setLoadingMessage("");
       setSavePercent(0);
@@ -94,8 +83,6 @@ export default function ModalAddDocument() {
       setSavePercent(0);
     }
   };
-
-  console.log("fileList", fileList);
 
   return (
     <Modal
@@ -124,8 +111,24 @@ export default function ModalAddDocument() {
               ))}
           </Select>
         </Form.Item>
-        <UploaderInput />
+        <UploaderInput
+          setLoading={setLoading}
+          setLoadingMessage={setLoadingMessage}
+          setUploadPercent={setUploadPercent}
+        />
         <Form.Item>
+          {loading && (
+            <div style={{ marginBottom: 16 }}>
+              <Spin tip={loadingMessage} />
+              <Progress
+                percent={
+                  loadingMessage === "Пожалуйста, подождите, файл загружается"
+                    ? uploadPercent
+                    : savePercent
+                }
+              />
+            </div>
+          )}
           <Button type="primary" htmlType="submit" disabled={loading}>
             Сохранить файлы
           </Button>
@@ -134,6 +137,143 @@ export default function ModalAddDocument() {
     </Modal>
   );
 }
+
+// import React, { useState } from "react";
+// import {
+//   Modal,
+//   Button,
+//   Upload,
+//   message,
+//   Form,
+//   Select,
+//   Spin,
+//   Progress,
+// } from "antd";
+// import config from "../../../config";
+// import axios from "axios";
+// import useDocuments from "../../../stores/Cabinet/useDocuments";
+// import { UploadOutlined } from "@ant-design/icons";
+// import UploaderInput from "../../FormComponents/UploaderInput";
+// const { Option } = Select;
+
+// export default function ModalAddDocument() {
+//   const openModalAdd = useDocuments((state) => state.openModalAdd);
+//   const setOpenModalAdd = useDocuments((state) => state.setOpenModalAdd);
+//   const fetchDocuments = useDocuments((state) => state.fetchDocuments);
+//   const nameDocs = useDocuments((state) => state.nameDocs);
+//   const [fileList, setFileList] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [loadingMessage, setLoadingMessage] = useState("");
+//   const [uploadPercent, setUploadPercent] = useState(0);
+//   const [savePercent, setSavePercent] = useState(0);
+
+//   const [form] = Form.useForm();
+//   const handleModalClose = () => {
+//     setOpenModalAdd(false);
+//     form.resetFields();
+//     setFileList([]);
+//   };
+
+//   const handleSaveDocument = async (values) => {
+//     console.log(form.getFieldValue("fileDoc"));
+//     try {
+//       setLoading(true);
+//       setLoadingMessage("Пожалуйста, подождите, файл сохраняется");
+
+//       const formData = {
+//         documentName: values.documentName,
+//         nameDoc_Key: values.documentName,
+//         files: form.getFieldValue("fileDoc").map((file) => ({
+//           name: file,
+//         })),
+//       };
+
+//       const token = localStorage.getItem("jwt");
+
+//       // Имитация процесса сохранения
+//       let percent = 0;
+//       const interval = setInterval(() => {
+//         percent += 20; // Увеличиваем процент на 20 каждый раз
+//         setSavePercent(percent);
+//         if (percent >= 100) {
+//           clearInterval(interval);
+//         }
+//       }, 200); // Обновляем каждые 200 миллисекунд
+
+//       const response = await axios.post(
+//         `${config.backServer}/api/cabinet/documents`,
+//         formData,
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//           withCredentials: true,
+//         }
+//       );
+
+//       clearInterval(interval);
+//       setSavePercent(100);
+
+//       console.log("response", response);
+//       message.success("Документ успешно сохранен");
+
+//       fetchDocuments();
+
+//       setOpenModalAdd(false);
+//       form.resetFields();
+//       setFileList([]);
+//       setLoading(false);
+//       setLoadingMessage("");
+//       setSavePercent(0);
+//     } catch (error) {
+//       console.error("Ошибка при сохранении документа:", error);
+//       message.error("Не удалось сохранить документ");
+//       setLoading(false);
+//       setLoadingMessage("");
+//       setSavePercent(0);
+//     }
+//   };
+
+//   console.log("fileList", fileList);
+
+//   return (
+//     <Modal
+//       title="Загрузить новый документ"
+//       open={openModalAdd}
+//       onCancel={handleModalClose}
+//       footer={null}
+//     >
+//       <Form form={form} onFinish={handleSaveDocument}>
+//         <Form.Item
+//           label="Название"
+//           name="documentName"
+//           rules={[
+//             {
+//               required: true,
+//               message: "Пожалуйста, выберите название документа",
+//             },
+//           ]}
+//         >
+//           <Select>
+//             {nameDocs &&
+//               nameDocs.map((nameDocs, index) => (
+//                 <Option key={index} value={nameDocs.Ref_Key}>
+//                   {nameDocs.Description}
+//                 </Option>
+//               ))}
+//           </Select>
+//         </Form.Item>
+//         <UploaderInput />
+//         <Form.Item>
+//           <Button type="primary" htmlType="submit" disabled={loading}>
+//             Сохранить файлы
+//           </Button>
+//         </Form.Item>
+//       </Form>
+//     </Modal>
+//   );
+// }
 
 // import React, { useEffect, useState } from 'react'
 // import {
