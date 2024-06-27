26405ff5a19378ce19634793b9740af00537757d
Stanislav <yanut995@gmail.com>
2024-06-27 14:15:59 +0300

    Гарри Поттер и срочный коммит калькулятора

--- src/pages/Calc/Calc.js
+++ src/pages/Calc/Calc.js
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
 import AppHelmet from "../../components/Global/AppHelmet";
-import {
-  Typography,
-  InputNumber,
-  Button,
-  Form,
-  Table,
-  Tooltip,
-  Select,
-} from "antd";
+import { Typography, InputNumber, Button, Form, Table, Tooltip } from "antd";
 import TweenOne from "rc-tween-one";
 import Children from "rc-tween-one/lib/plugin/ChildrenPlugin";
 import jsonData from "./powerData.json";
@@ -18,32 +10,30 @@ import { formItemLayoutForCalc } from "../../components/configSizeForm";
 TweenOne.plugins.push(Children);
 
 const { Title } = Typography;
-const { Option } = Select;
 
 const formItemLayout = formItemLayoutForCalc;
 
 export default function Calc() {
   const [form] = Form.useForm();
   const [totalPower, setTotalPower] = useState(0);
-  console.log(totalPower);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedDevice, setSelectedDevice] = useState(null);
   const [animation, setAnimation] = useState(null);
 
   const handleFinish = (values) => {
     let total = 0;
-    selectedItems.forEach((item) => {
-      const inputValue = parseFloat(values[item.key].value);
-      const countValue = parseFloat(values[item.key].count) || 1;
-      const formula = item.formula;
+    jsonData.forEach((section, sectionIndex) => {
+      section.items.forEach((item, itemIndex) => {
+        const key = `${sectionIndex}-${itemIndex}`;
+        const inputValue = parseFloat(values[key].value);
+        const countValue = parseFloat(values[key].count) || 1;
+        const formula = item.formula;
 
-      if (!isNaN(inputValue) && formula) {
-        const result = eval(
-          formula.replace("count", countValue).replace("value", inputValue)
-        );
-        total += result;
-      }
+        if (!isNaN(inputValue) && formula) {
+          const result = eval(
+            formula.replace("count", countValue).replace("value", inputValue)
+          );
+          total += result;
+        }
+      });
     });
 
     setAnimation({
@@ -54,70 +44,69 @@ export default function Calc() {
     setTotalPower(total.toFixed(2));
   };
 
-  const handleAddDevice = () => {
-    if (selectedCategory !== null && selectedDevice !== null) {
-      const device = jsonData[selectedCategory].items[selectedDevice];
-      setSelectedItems([
-        ...selectedItems,
-        {
-          key: `${selectedCategory}-${selectedDevice}-${selectedItems.length}`,
-          name: device.name,
-          formula: device.formula,
-          defaultValue: device.defaultValue,
-          description: device.description,
-        },
-      ]);
-      setSelectedCategory(null);
-      setSelectedDevice(null);
-    }
-  };
+  const dataSource = jsonData.reduce((acc, section, sectionIndex) => {
+    const sectionItems = section.items.map((item, itemIndex) => ({
+      key: `${sectionIndex}-${itemIndex}`,
+      name: item.name,
+      value: item.defaultValue,
+      count: 1,
+      formula: item.formula,
+      description: item.description,
+    }));
+    return [
+      ...acc,
+      {
+        key: `section-${sectionIndex}`,
+        section: section.section,
+        isSection: true,
+      },
+      ...sectionItems,
+    ];
+  }, []);
 
-  const columns = [
+  const renderColumns = [
     {
       title: "Название",
       dataIndex: "name",
       key: "name",
-      render: (text, record) => (
-        <Tooltip title={record.description || ""}>
-          <span>
-            {text} {record.description && <span>ℹ️</span>}
-          </span>
-        </Tooltip>
-      ),
+      render: (text, record) =>
+        record.isSection ? (
+          <strong>{record.section}</strong>
+        ) : (
+          <Tooltip title={record.description || ""}>
+            <span>
+              {text} {record.description && <span>ℹ️</span>}
+            </span>
+          </Tooltip>
+        ),
     },
     {
       title: "Паспортная мощность (кВт)",
       dataIndex: "value",
       key: "value",
-      render: (_, record) => (
-        <Form.Item
-          name={[record.key, "value"]}
-          initialValue={record.defaultValue}
-          noStyle
-        >
-          <InputNumber min={0} step={0.01} stringMode />
-        </Form.Item>
-      ),
+      render: (_, record) =>
+        !record.isSection && (
+          <Form.Item
+            name={[record.key, "value"]}
+            initialValue={record.value || record.defaultValue}
+            noStyle
+          >
+            <InputNumber min={0} step={0.01} stringMode />
+          </Form.Item>
+        ),
     },
     {
       title: "Количество",
       dataIndex: "count",
       key: "count",
-      render: (_, record) => (
-        <Form.Item name={[record.key, "count"]} initialValue={1} noStyle>
-          <InputNumber min={0} step={1} stringMode />
-        </Form.Item>
-      ),
+      render: (_, record) =>
+        !record.isSection && (
+          <Form.Item name={[record.key, "count"]} initialValue={1} noStyle>
+            <InputNumber min={0} step={1} stringMode />
+          </Form.Item>
+        ),
     },
   ];
-
-  const categoryOptions = jsonData.map((section, sectionIndex) => ({
-    label: section.section,
-    options: section.items.map((item, itemIndex) => ({
-      label: item.name,
-      value: `${sectionIndex}-${itemIndex}`,
-    })),
-  }));
 
   return (
     <>
@@ -125,30 +114,9 @@ export default function Calc() {
       <div>
         <Title level={1}>Калькулятор мощности</Title>
         <Form form={form} onFinish={handleFinish} {...formItemLayout} labelWrap>
-          <Form.Item label="Выберите категорию и устройство">
-            <Select
-              value={
-                selectedCategory !== null && selectedDevice !== null
-                  ? `${selectedCategory}-${selectedDevice}`
-                  : undefined
-              }
-              onChange={(value) => {
-                const [category, device] = value.split("-");
-                setSelectedCategory(category);
-                setSelectedDevice(device);
-              }}
-              options={categoryOptions}
-              placeholder="Выберите устройство"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" onClick={handleAddDevice}>
-              Добавить устройство
-            </Button>
-          </Form.Item>
           <Table
-            columns={columns}
-            dataSource={selectedItems}
+            columns={renderColumns}
+            dataSource={dataSource}
             pagination={false}
             locale={{ emptyText: "Нет данных" }}
           />
@@ -175,6 +143,7 @@ export default function Calc() {
   );
 }
 
+//Старый калькулятор на 27.06 13:40
 // import React, { useState } from "react";
 // import AppHelmet from "../../components/Global/AppHelmet";
 // import {
@@ -185,11 +154,14 @@ export default function Calc() {
 //   Table,
 //   Tooltip,
 //   Select,
-//   Spin,
 // } from "antd";
+// import TweenOne from "rc-tween-one";
+// import Children from "rc-tween-one/lib/plugin/ChildrenPlugin";
 // import jsonData from "./powerData.json";
 // import styles from "./Calc.module.css";
 // import { formItemLayoutForCalc } from "../../components/configSizeForm";
+
+// TweenOne.plugins.push(Children);
 
 // const { Title } = Typography;
 // const { Option } = Select;
@@ -199,30 +171,33 @@ export default function Calc() {
 // export default function Calc() {
 //   const [form] = Form.useForm();
 //   const [totalPower, setTotalPower] = useState(0);
-//   const [loading, setLoading] = useState(false);
+//   console.log(totalPower);
 //   const [selectedItems, setSelectedItems] = useState([]);
 //   const [selectedCategory, setSelectedCategory] = useState(null);
 //   const [selectedDevice, setSelectedDevice] = useState(null);
+//   const [animation, setAnimation] = useState(null);
 
 //   const handleFinish = (values) => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       let total = 0;
-//       selectedItems.forEach((item) => {
-//         const inputValue = parseFloat(values[item.key].value);
-//         const countValue = parseFloat(values[item.key].count) || 1;
-//         const formula = item.formula;
+//     let total = 0;
+//     selectedItems.forEach((item) => {
+//       const inputValue = parseFloat(values[item.key].value);
+//       const countValue = parseFloat(values[item.key].count) || 1;
+//       const formula = item.formula;
 
-//         if (!isNaN(inputValue) && formula) {
-//           const result = eval(
-//             formula.replace("count", countValue).replace("value", inputValue)
-//           );
-//           total += result;
-//         }
-//       });
-//       setTotalPower(total.toFixed(2));
-//       setLoading(false);
-//     }, 2000);
+//       if (!isNaN(inputValue) && formula) {
+//         const result = eval(
+//           formula.replace("count", countValue).replace("value", inputValue)
+//         );
+//         total += result;
+//       }
+//     });
+
+//     setAnimation({
+//       Children: { value: total, floatLength: 2, formatMoney: false },
+//       duration: 3000,
+//     });
+
+//     setTotalPower(total.toFixed(2));
 //   };
 
 //   const handleAddDevice = () => {
@@ -321,230 +296,24 @@ export default function Calc() {
 //             columns={columns}
 //             dataSource={selectedItems}
 //             pagination={false}
+//             locale={{ emptyText: "Нет данных" }}
 //           />
 //           <Form.Item>
-//             <Button type="primary" htmlType="submit" disabled={loading}>
-//               {loading ? <Spin /> : "Рассчитать"}
+//             <Button
+//               type="primary"
+//               htmlType="submit"
+//               className={styles.calculateButton}
+//             >
+//               Рассчитать
 //             </Button>
 //           </Form.Item>
 //         </Form>
 //         <div className={styles.totalPower}>
 //           <Title level={4}>
-//             Итого требуемая электрическая мощность (оценочно): {totalPower} кВт
-//           </Title>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// import React, { useState } from "react";
-// import AppHelmet from "../../components/Global/AppHelmet";
-// import { Typography, InputNumber, Button, Form, Collapse, Select, Tooltip } from "antd";
-// import jsonData from "./powerData.json";
-// import styles from "./Calc.module.css";
-// import { formItemLayoutForCalc } from '../../components/configSizeForm';
-
-// const { Title } = Typography;
-// const { Panel } = Collapse;
-// const { Option } = Select;
-// const formItemLayout = formItemLayoutForCalc;
-
-// export default function Calc() {
-//   const [form] = Form.useForm();
-//   const [totalPower, setTotalPower] = useState(0);
-//   const [loading, setLoading] = useState(false);
-//   const [selectedCategory, setSelectedCategory] = useState(null);
-//   const [selectedItem, setSelectedItem] = useState(null);
-
-//   const handleFinish = (values) => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       let total = 0;
-//       Object.keys(values).forEach((key) => {
-//         const [sectionIndex, itemIndex] = key.split("-");
-//         const item = jsonData[sectionIndex].items[itemIndex];
-//         const inputValue = parseFloat(values[key]);
-//         const countKey = `${key}-count`;
-//         const countValue = parseFloat(values[countKey]) || 1;
-
-//         if (!isNaN(inputValue) && item.formula) {
-//           const result = eval(
-//             item.formula
-//               .replace("count", countValue)
-//               .replace("area", inputValue)
-//               .replace("length", inputValue)
-//           );
-//           total += result;
-//         }
-//       });
-//       setTotalPower(total.toFixed(2));
-//       setLoading(false);
-//     }, 2000);
-//   };
-
-//   const handleCategoryChange = (value) => {
-//     setSelectedCategory(value);
-//     setSelectedItem(null);
-//     form.resetFields();
-//   };
-
-//   const handleItemChange = (value) => {
-//     setSelectedItem(value);
-//   };
-
-//   const selectedItems = selectedCategory !== null ? jsonData[selectedCategory].items : [];
-
-//   return (
-//     <>
-//       <AppHelmet title={"Калькулятор"} desc={"Калькулятор мощности"} />
-//       <div>
-//         <Title level={1}>Калькулятор мощности</Title>
-//         <Form form={form} onFinish={handleFinish} {...formItemLayout} labelWrap>
-//           <Form.Item label="Выберите категорию">
-//             <Select onChange={handleCategoryChange} placeholder="Категория">
-//               {jsonData.map((section, index) => (
-//                 <Option key={index} value={index}>
-//                   {section.section}
-//                 </Option>
-//               ))}
-//             </Select>
-//           </Form.Item>
-//           {selectedCategory !== null && (
-//             <Form.Item label="Выберите тип">
-//               <Select onChange={handleItemChange} placeholder="Тип">
-//                 {selectedItems.map((item, index) => (
-//                   <Option key={index} value={index}>
-//                     {item.name}
-//                   </Option>
-//                 ))}
-//               </Select>
-//             </Form.Item>
-//           )}
-//           {selectedItem !== null && (
-//             <Collapse>
-//               <Panel header={selectedItems[selectedItem].name} key={selectedItem}>
-//                 <div className={styles.inputGroup}>
-//                   <Tooltip title={selectedItems[selectedItem].description || ""}>
-//                     <Form.Item
-//                       label={selectedItems[selectedItem].name}
-//                       name={`${selectedCategory}-${selectedItem}`}
-//                       initialValue={selectedItems[selectedItem].defaultValue}
-//                     >
-//                       <InputNumber min={0} step={0.01} stringMode />
-//                     </Form.Item>
-//                   </Tooltip>
-//                   <Form.Item
-//                     label="Количество"
-//                     name={`${selectedCategory}-${selectedItem}-count`}
-//                     initialValue={1}
-//                   >
-//                     <InputNumber min={0} step={1} stringMode />
-//                   </Form.Item>
-//                 </div>
-//               </Panel>
-//             </Collapse>
-//           )}
-//           <Form.Item>
-//             <Button type="primary" htmlType="submit" disabled={loading}>
-//               {loading ? "Рассчитываю..." : "Рассчитать"}
-//             </Button>
-//           </Form.Item>
-//         </Form>
-//         <div className={styles.totalPower}>
-//           <Title level={4}>
-//             Итого требуемая электрическая мощность (оценочно): {totalPower} кВт
-//           </Title>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-//Старый калькулятор
-// import React, { useState } from "react";
-// import AppHelmet from "../../components/Global/AppHelmet";
-// import { Typography, InputNumber, Button, Form, Collapse, Spin } from "antd";
-// import jsonData from "./powerData.json";
-// import styles from "./Calc.module.css";
-// import { formItemLayoutForCalc } from '../../components/configSizeForm'
-
-// const { Title } = Typography;
-// const { Panel } = Collapse;
-// const formItemLayout = formItemLayoutForCalc
-// export default function Calc() {
-//   const [form] = Form.useForm();
-//   const [totalPower, setTotalPower] = useState(0);
-//   const [loading, setLoading] = useState(false);
-
-//   const handleFinish = (values) => {
-//     setLoading(true);
-//     setTimeout(() => {
-//       let total = 0;
-//       Object.keys(values).forEach((key) => {
-//         const [sectionIndex, itemIndex] = key.split("-");
-//         const item = jsonData[sectionIndex].items[itemIndex];
-//         const inputValue = parseFloat(values[key]);
-//         const countKey = `${key}-count`;
-//         const countValue = parseFloat(values[countKey]) || 1;
-
-//         if (!isNaN(inputValue) && item.formula) {
-//           const result = eval(
-//             item.formula
-//               .replace("count", countValue)
-//               .replace("area", inputValue)
-//               .replace("length", inputValue)
-//           );
-//           total += result;
-//         }
-//       });
-//       setTotalPower(total.toFixed(2));
-//       setLoading(false);
-//     }, 2000); // Задержка для имитации расчета
-//   };
-
-//   return (
-//     <>
-//       <AppHelmet title={"Калькулятор"} desc={"Калькулятор мощности"} />
-//       <div>
-//         <Title level={1}>Калькулятор мощности</Title>
-//         <Form form={form} onFinish={handleFinish} {...formItemLayout} labelWrap>
-//           <Collapse>
-//             {jsonData.map((section, sectionIndex) => (
-//               <Panel header={section.section} key={sectionIndex}>
-//                 {section.items.map((item, itemIndex) => (
-//                   <div
-//                     key={`${sectionIndex}-${itemIndex}`}
-//                     className={styles.inputGroup}
-//                   >
-//                     <Form.Item
-//                       label={item.name}
-//                       name={`${sectionIndex}-${itemIndex}`}
-//                       key={`${sectionIndex}-${itemIndex}`}
-//                     >
-//                       <InputNumber min={0} step={0.01} stringMode />
-//                     </Form.Item>
-//                     <Form.Item
-//                       label="Количество"
-//                       name={`${sectionIndex}-${itemIndex}-count`}
-//                       key={`${sectionIndex}-${itemIndex}-count`}
-//                     >
-//                       <InputNumber min={0} step={1} stringMode />
-//                     </Form.Item>
-//                   </div>
-//                 ))}
-//               </Panel>
-//             ))}
-//           </Collapse>
-//           <Form.Item>
-//             <Button type="primary" htmlType="submit" disabled={loading}>
-//               {loading ? <Spin /> : "Рассчитать"}
-//             </Button>
-//           </Form.Item>
-//         </Form>
-//         <div className={styles.totalPower}>
-//           <Title level={4}>
-//             Итого требуемая электрическая мощность (оценочно): {totalPower} кВт
+//             Итого требуемая электрическая мощность (оценочно):{" "}
+//             <TweenOne animation={animation} style={{ fontSize: 24 }}>
+//               {totalPower} кВт
+//             </TweenOne>
 //           </Title>
 //         </div>
 //       </div>
