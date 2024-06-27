b37d3d26432160cf1850630e784d7820b4c9c82b
luxeivan <luxeivan@gmail.com>
2024-06-27 17:19:34 +0300

    очередной коммит

--- src/pages/Cabinet/Claims/Claims.js
+++ src/pages/Cabinet/Claims/Claims.js
@@ -1,17 +1,30 @@
 import { Skeleton } from 'antd'
 import Title from 'antd/es/typography/Title'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import useNewServicetest from '../../../stores/Cabinet/useNewServicetest'
 
 export default function Claims() {
+    const claims = useNewServicetest(state => state.claims)
+    const fetchClaims = useNewServicetest(state => state.fetchClaims)
+    useEffect(() => {
+        fetchClaims()
+    }, [])
     const { id } = useParams()
+    console.log(claims)
     return (
-        <div>
-            <Title level={1}>Заявки ЛКЗ №{id}</Title>
-            
-            <Skeleton active avatar paragraph={{ rows: 2 }} />
-            <Skeleton active avatar paragraph={{ rows: 2 }} />
-            <Skeleton active avatar paragraph={{ rows: 2 }} />
-        </div>
+        <>
+            {!claims &
+
+                <div>
+                    <Title level={1}>Заявки ЛКЗ №{id}</Title>
+
+                    <Skeleton active avatar paragraph={{ rows: 2 }} />
+                    <Skeleton active avatar paragraph={{ rows: 2 }} />
+                    <Skeleton active avatar paragraph={{ rows: 2 }} />
+                </div>
+            }
+            {claims&&<div>Загрузились</div>}
+        </>
     )
 }
