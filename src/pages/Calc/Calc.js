3db4d2b7908fa97d4f9fd9a5b92f91854a5606bb
Stanislav <yanut995@gmail.com>
2024-06-28 13:04:00 +0300

    Гарри Поттер и небольшие косметические правки

--- src/pages/Calc/Calc.js
+++ src/pages/Calc/Calc.js
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import AppHelmet from "../../components/Global/AppHelmet";
 import { Typography, InputNumber, Button, Form, Table, Tooltip } from "antd";
 import TweenOne from "rc-tween-one";
 import Children from "rc-tween-one/lib/plugin/ChildrenPlugin";
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
 import jsonData from "./powerData.json";
+import useCalc from "../../stores/useCalc";
 import styles from "./Calc.module.css";
 import { formItemLayoutForCalc } from "../../components/configSizeForm";
 
 // Добавляем плагин для анимации чисел
 TweenOne.plugins.push(Children);
-
-// Инициализируем шрифты для pdfMake
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const { Title, Paragraph } = Typography;
 
@@ -21,105 +17,7 @@ const formItemLayout = formItemLayoutForCalc;
 
 export default function Calc() {
   const [form] = Form.useForm();
-  const [totalPower, setTotalPower] = useState(0);
-  const [animation, setAnimation] = useState(null);
-  const [calculatedData, setCalculatedData] = useState({});
-
-  // Функция, которая вызывается при отправке формы
-  const handleFinish = (values) => {
-    let total = 0;
-    const tableData = [];
-    const newCalculatedData = {};
-
-    // Перебираем каждый раздел и элемент в jsonData
-    jsonData.forEach((section, sectionIndex) => {
-      section.items.forEach((item, itemIndex) => {
-        const key = `${sectionIndex}-${itemIndex}`;
-        const inputValue = parseFloat(values[key].value);
-        const countValue = parseFloat(values[key].count) || 1;
-        const usageCoefficient =
-          section.section === "Электроприборы инженерного назначения"
-            ? 0.6
-            : 0.3;
-        const formula = item.formula;
-
-        // Проверяем, что значение введено корректно и есть формула
-        if (!isNaN(inputValue) && formula) {
-          // Вычисляем результат по формуле
-          const result = eval(
-            formula.replace("count", countValue).replace("value", inputValue)
-          );
-          // Вычисляем потребляемую мощность
-          const consumedPower = inputValue * countValue * usageCoefficient; // Исправленный расчет потребляемой мощности
-          total += consumedPower;
-          // Добавляем данные для таблицы
-          tableData.push([
-            item.name,
-            inputValue.toFixed(2),
-            countValue.toFixed(0),
-            usageCoefficient.toFixed(2),
-            consumedPower.toFixed(2),
-          ]);
-
-          newCalculatedData[key] = consumedPower.toFixed(2);
-        }
-      });
-    });
-
-    // Настраиваем анимацию для отображения итоговой мощности
-    setAnimation({
-      Children: { value: total, floatLength: 2, formatMoney: false },
-      duration: 3000,
-    });
-
-    // Устанавливаем итоговую мощность
-    setTotalPower(total.toFixed(2));
-    setCalculatedData(newCalculatedData);
-    // Генерируем PDF с результатами
-    generatePDF(tableData, total);
-  };
-
-  // Функция для генерации PDF
-  const generatePDF = (tableData, total) => {
-    const docDefinition = {
-      content: [
-        { text: "Калькулятор мощности", style: "header" },
-        {
-          table: {
-            headerRows: 1,
-            widths: ["*", "auto", "auto", "auto", "auto"],
-            body: [
-              [
-                "Название",
-                "Мощность (кВт)",
-                "Количество",
-                "Коэффициент использования",
-                "Потребляемая мощность (кВт)",
-              ],
-              ...tableData,
-            ],
-          },
-        },
-        {
-          text: `Итого требуемая электрическая мощность (оценочно): ${total} кВт`,
-          style: "total",
-        },
-      ],
-      styles: {
-        header: {
-          fontSize: 18,
-          bold: true,
-          margin: [0, 0, 0, 10],
-        },
-        total: {
-          margin: [0, 10, 0, 0],
-        },
-      },
-    };
-
-    // Скачиваем PDF
-    pdfMake.createPdf(docDefinition).download("Калькулятор мощности.pdf");
-  };
+  const { totalPower, animation, calculatedData, handleFinish } = useCalc();
 
   // Подготавливаем данные для таблицы и сортируем элементы в каждом разделе по алфавиту
   const dataSource = jsonData.reduce((acc, section, sectionIndex) => {
@@ -277,15 +175,19 @@ export default function Calc() {
 // import { Typography, InputNumber, Button, Form, Table, Tooltip } from "antd";
 // import TweenOne from "rc-tween-one";
 // import Children from "rc-tween-one/lib/plugin/ChildrenPlugin";
-// import jsPDF from "jspdf";
-// import "jspdf-autotable";
+// import pdfMake from "pdfmake/build/pdfmake";
+// import pdfFonts from "pdfmake/build/vfs_fonts";
 // import jsonData from "./powerData.json";
 // import styles from "./Calc.module.css";
 // import { formItemLayoutForCalc } from "../../components/configSizeForm";
 
+// // Добавляем плагин для анимации чисел
 // TweenOne.plugins.push(Children);
 
-// const { Title } = Typography;
+// // Инициализируем шрифты для pdfMake
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+// const { Title, Paragraph } = Typography;
 
 // const formItemLayout = formItemLayoutForCalc;
 
@@ -293,64 +195,120 @@ export default function Calc() {
 //   const [form] = Form.useForm();
 //   const [totalPower, setTotalPower] = useState(0);
 //   const [animation, setAnimation] = useState(null);
+//   const [calculatedData, setCalculatedData] = useState({});
 
+//   // Функция, которая вызывается при отправке формы
 //   const handleFinish = (values) => {
 //     let total = 0;
 //     const tableData = [];
+//     const newCalculatedData = {};
+
+//     // Перебираем каждый раздел и элемент в jsonData
 //     jsonData.forEach((section, sectionIndex) => {
 //       section.items.forEach((item, itemIndex) => {
 //         const key = `${sectionIndex}-${itemIndex}`;
 //         const inputValue = parseFloat(values[key].value);
 //         const countValue = parseFloat(values[key].count) || 1;
+//         const usageCoefficient =
+//           section.section === "Электроприборы инженерного назначения"
+//             ? 0.6
+//             : 0.3;
 //         const formula = item.formula;
 
+//         // Проверяем, что значение введено корректно и есть формула
 //         if (!isNaN(inputValue) && formula) {
+//           // Вычисляем результат по формуле
 //           const result = eval(
 //             formula.replace("count", countValue).replace("value", inputValue)
 //           );
-//           total += result;
-//           tableData.push({
-//             name: item.name,
-//             value: inputValue.toFixed(2),
-//             count: countValue.toFixed(0),
-//             result: result.toFixed(2)
-//           });
+//           // Вычисляем потребляемую мощность
+//           const consumedPower = inputValue * countValue * usageCoefficient; // Исправленный расчет потребляемой мощности
+//           total += consumedPower;
+//           // Добавляем данные для таблицы
+//           tableData.push([
+//             item.name,
+//             inputValue.toFixed(2),
+//             countValue.toFixed(0),
+//             usageCoefficient.toFixed(2),
+//             consumedPower.toFixed(2),
+//           ]);
+
+//           newCalculatedData[key] = consumedPower.toFixed(2);
 //         }
 //       });
 //     });
 
+//     // Настраиваем анимацию для отображения итоговой мощности
 //     setAnimation({
 //       Children: { value: total, floatLength: 2, formatMoney: false },
 //       duration: 3000,
 //     });
 
+//     // Устанавливаем итоговую мощность
 //     setTotalPower(total.toFixed(2));
+//     setCalculatedData(newCalculatedData);
+//     // Генерируем PDF с результатами
 //     generatePDF(tableData, total);
 //   };
 
+//   // Функция для генерации PDF
 //   const generatePDF = (tableData, total) => {
-//     const doc = new jsPDF();
+//     const docDefinition = {
+//       content: [
+//         { text: "Калькулятор мощности", style: "header" },
+//         {
+//           table: {
+//             headerRows: 1,
+//             widths: ["*", "auto", "auto", "auto", "auto"],
+//             body: [
+//               [
+//                 "Название",
+//                 "Мощность (кВт)",
+//                 "Количество",
+//                 "Коэффициент использования",
+//                 "Потребляемая мощность (кВт)",
+//               ],
+//               ...tableData,
+//             ],
+//           },
+//         },
+//         {
+//           text: `Итого требуемая электрическая мощность (оценочно): ${total} кВт`,
+//           style: "total",
+//         },
+//       ],
+//       styles: {
+//         header: {
+//           fontSize: 18,
+//           bold: true,
+//           margin: [0, 0, 0, 10],
+//         },
+//         total: {
+//           margin: [0, 10, 0, 0],
+//         },
+//       },
+//     };
 
-//     doc.text("Калькулятор мощности", 14, 20);
-//     doc.autoTable({
-//       head: [['Название', 'Мощность (кВт)', 'Количество', 'Результат (кВт)']],
-//       body: tableData.map(item => [item.name, item.value, item.count, item.result]),
-//       startY: 30
-//     });
-//     doc.text(`Итого требуемая электрическая мощность (оценочно): ${total} кВт`, 14, doc.autoTable.previous.finalY + 10);
-
-//     doc.save("calculation.pdf");
+//     // Скачиваем PDF
+//     pdfMake.createPdf(docDefinition).download("Калькулятор мощности.pdf");
 //   };
 
+//   // Подготавливаем данные для таблицы и сортируем элементы в каждом разделе по алфавиту
 //   const dataSource = jsonData.reduce((acc, section, sectionIndex) => {
-//     const sectionItems = section.items.map((item, itemIndex) => ({
-//       key: `${sectionIndex}-${itemIndex}`,
-//       name: item.name,
-//       value: item.defaultValue,
-//       count: 1,
-//       formula: item.formula,
-//       description: item.description,
-//     }));
+//     const sectionItems = section.items
+//       .sort((a, b) => a.name.localeCompare(b.name))
+//       .map((item, itemIndex) => ({
+//         key: `${sectionIndex}-${itemIndex}`,
+//         name: item.name,
+//         value: item.defaultValue,
+//         count: 1,
+//         usageCoefficient:
+//           section.section === "Электроприборы инженерного назначения"
+//             ? 0.6
+//             : 0.3,
+//         formula: item.formula,
+//         description: item.description,
+//       }));
 //     return [
 //       ...acc,
 //       {
@@ -362,6 +320,7 @@ export default function Calc() {
 //     ];
 //   }, []);
 
+//   // Настраиваем столбцы для таблицы
 //   const renderColumns = [
 //     {
 //       title: "Название электрооборудования",
@@ -408,13 +367,52 @@ export default function Calc() {
 //           </Form.Item>
 //         ),
 //     },
+//     {
+//       title: "Коэффициент использования",
+//       dataIndex: "usageCoefficient",
+//       key: "usageCoefficient",
+//       render: (text, record) =>
+//         !record.isSection && <span>{record.usageCoefficient.toFixed(1)}</span>,
+//     },
+//     {
+//       title: "Потребляемая мощность (кВт)",
+//       dataIndex: "consumedPower",
+//       key: "consumedPower",
+//       render: (text, record) => {
+//         if (record.isSection) {
+//           return null;
+//         }
+//         const key = `${record.key}`;
+//         return calculatedData[key] || null;
+//       },
+//     },
 //   ];
 
 //   return (
 //     <>
 //       <AppHelmet title={"Калькулятор"} desc={"Калькулятор мощности"} />
 //       <div className={styles.container}>
-//         <Title level={1}>Калькулятор мощности</Title>
+//         <Tooltip
+//           title="Предлагаемый    расчет выполнен для подключения к электрическим сетям по III категории надежности. Для электроприемников третьей категории электроснабжение может выполняться от одного источника питания при условии, что перерывы электроснабжения, необходимые для ремонта или замены поврежденного элемента системы электроснабжения, не превышают 1 суток.
+// Электроприемники второй категории в нормальных режимах должны обеспечиваться электроэнергией от двух независимых взаимно резервирующих источников питания. Для электроприемников второй категории при нарушении электроснабжения от одного из источников питания допустимы перерывы электроснабжения на время, необходимое для включения резервного питания действиями дежурного персонала или выездной оперативной бригад"
+//         >
+//           <span>
+//             <Title level={2}>Калькулятор мощности ℹ️</Title>{" "}
+//           </span>
+//         </Tooltip>
+//         <Paragraph style={{ textAlign: "justify", marginBottom: "20px" }}>
+//           Только для некоммерческого применения. Для заявителей - физических
+//           лиц. Результата расчёта является ориентировочным. Позволяет оценить
+//           мощность электрооборудования индивидуального домохозяйства (объекта с
+//           бытовым характером нагрузки), необходимую для технологического
+//           присоединения к электросети АО "Мособлэнерго". Для получения
+//           результата указать основные параметры домохозяйства и количество
+//           имеющихся электроприборов. Предложенное значение паспортной мощности
+//           электроприбора является средней величиной. Для уточнения можно
+//           установить свою величину паспортной мощности электроприбора. Расчет
+//           производится с применением коэффициентов использования
+//           электроприборов.
+//         </Paragraph>
 //         <Form form={form} onFinish={handleFinish} {...formItemLayout} labelWrap>
 //           <Table
 //             columns={renderColumns}
