3453347a23374fef75ea35477a21d10e48b06ae0
Stanislav <yanut995@gmail.com>
2024-07-03 14:08:06 +0300

    Гарри Поттер и большой рефакторинг

--- src/pages/Calc/helpers.js
+++ src/pages/Calc/helpers.js
@@ -10,14 +10,18 @@ export const prepareDataSource = () => {
         name: item.name,
         value: item.defaultValue,
         count: 1,
-        unit: item.unit || item.defaultUnit || "Штук", // Добавляем unit из JSON или дефолтное значение
+        unit: item.unit || item.defaultUnit || "Штук",
         usageCoefficient:
-          section.section === "Электроприборы инженерного назначения"
+          section.section === "Электроприборы инженерного назначения" ||
+          section.section === "Электроприборы бытового назначения"
             ? 0.6
             : 0.3,
         formula: item.formula,
         description: item.description,
-        fixedUnit: !!item.unit, // Устанавливаем флаг, если unit задан в JSON
+        fixedUnit:
+          !!item.unit ||
+          section.section === "Электроприборы инженерного назначения" ||
+          section.section === "Электроприборы бытового назначения",
       }));
     return [
       ...acc,
@@ -30,3 +34,36 @@ export const prepareDataSource = () => {
     ];
   }, []);
 };
+
+// import jsonData from "./powerData.json";
+
+// // Подготавливаем данные для таблицы и сортируем элементы в каждом разделе по алфавиту
+// export const prepareDataSource = () => {
+//   return jsonData.reduce((acc, section, sectionIndex) => {
+//     const sectionItems = section.items
+//       .sort((a, b) => a.name.localeCompare(b.name))
+//       .map((item, itemIndex) => ({
+//         key: `${sectionIndex}-${itemIndex}`,
+//         name: item.name,
+//         value: item.defaultValue,
+//         count: 1,
+//         unit: item.unit || item.defaultUnit || "Штук", // Добавляем unit из JSON или дефолтное значение
+//         usageCoefficient:
+//           section.section === "Электроприборы инженерного назначения"
+//             ? 0.6
+//             : 0.3,
+//         formula: item.formula,
+//         description: item.description,
+//         fixedUnit: !!item.unit, // Устанавливаем флаг, если unit задан в JSON
+//       }));
+//     return [
+//       ...acc,
+//       {
+//         key: `section-${sectionIndex}`,
+//         section: section.section,
+//         isSection: true,
+//       },
+//       ...sectionItems,
+//     ];
+//   }, []);
+// };
