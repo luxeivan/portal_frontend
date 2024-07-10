2b31cced7d569495b5e3c8418430656210786cca
Stanislav <yanut995@gmail.com>
2024-07-10 16:30:16 +0300

    Небольой коммит с маской

--- src/components/FormComponentsNew/TextInput.js
+++ src/components/FormComponentsNew/TextInput.js
@@ -1,34 +1,66 @@
-import React from 'react'
-import { Button, Form, Input, InputNumber, message, Space } from 'antd';
+import React from "react";
+import { Form, Input } from "antd";
+import ReactInputMask from "react-input-mask";
 
-export default function TextInput({ name = 'name', label = 'Label', disabled = false, placeholder = 'Пример', required = false, dependOf = false, howDepend = false }) {
-    const form = Form.useFormInstance()
-    const fieldDepends = Form.useWatch(dependOf, form)
-    // console.log(name)
-    const formElement = (
-        <Form.Item
-            name={name}
-            label={label}
-            rules={[
-                {
-                    required: required,
-                    message: 'Это поле обязательное'
-                }
-            ]}
-        >
-            <Input placeholder={placeholder} disabled={disabled} />
-        </Form.Item>
-    )
-    if (!dependOf) return formElement
-    if (dependOf && howDepend && howDepend.values.length > 0) {
-        let show = false
-        howDepend.values.forEach(item => {
-            if (item.value === "true") item.value = true
-            if (item.value == fieldDepends) show = true
-        })
-        if (show) return formElement
-    }
-    if (dependOf && howDepend && howDepend.min && howDepend.max) {
-        if (fieldDepends >= howDepend.min && fieldDepends <= howDepend.max) return formElement
-    }
+export default function TextInput({
+  name = "name",
+  label = "Label",
+  disabled = false,
+  placeholder = "Пример",
+  required = false,
+  dependOf = false,
+  howDepend = false,
+  mask = null, // Маска по умолчанию для проверки
+}) {
+  const form = Form.useFormInstance();
+  const fieldDepends = Form.useWatch(dependOf, form);
+
+  const handleChange = (e) => {
+    form.setFieldsValue({ [name]: e.target.value });
+  };
+
+  const formElement = (
+    <Form.Item
+      name={name}
+      label={label}
+      rules={[
+        {
+          required: required,
+          message: "Это поле обязательное",
+        },
+      ]}
+    >
+      {mask ? (
+        <ReactInputMask mask={mask} onChange={handleChange}>
+          {(inputProps) => (
+            <Input
+              {...inputProps}
+              placeholder={placeholder}
+              disabled={disabled}
+            />
+          )}
+        </ReactInputMask>
+      ) : (
+        <Input
+          placeholder={placeholder}
+          disabled={disabled}
+          onChange={handleChange}
+        />
+      )}
+    </Form.Item>
+  );
+
+  if (!dependOf) return formElement;
+  if (dependOf && howDepend && howDepend.values.length > 0) {
+    let show = false;
+    howDepend.values.forEach((item) => {
+      if (item.value === "true") item.value = true;
+      if (item.value == fieldDepends) show = true;
+    });
+    if (show) return formElement;
+  }
+  if (dependOf && howDepend && howDepend.min && howDepend.max) {
+    if (fieldDepends >= howDepend.min && fieldDepends <= howDepend.max)
+      return formElement;
+  }
 }
--- src/pages/Cabinet/NewClaim.js
+++ src/pages/Cabinet/NewClaim.js
@@ -1,93 +1,151 @@
-import { Form, Typography, Button, Drawer, Descriptions } from 'antd'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import useNewClaim from '../../stores/Cabinet/useNewClaim'
-import TextInput from '../../components/FormComponentsNew/TextInput'
-import NumberInput from '../../components/FormComponentsNew/NumberInput'
-import SliderInput from '../../components/FormComponentsNew/SliderInput'
-import SelectInput from '../../components/FormComponentsNew/SelectInput'
-import DividerForm from '../../components/FormComponentsNew/DividerForm'
-import SubjectInput from '../../components/FormComponents/SubjectInput'
-import CheckboxInput from '../../components/FormComponents/CheckboxInput'
-import TableInput from '../../components/FormComponentsNew/TableInput'
+import { Form, Typography, Button, Drawer, Descriptions } from "antd";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useNewClaim from "../../stores/Cabinet/useNewClaim";
+import TextInput from "../../components/FormComponentsNew/TextInput";
+import NumberInput from "../../components/FormComponentsNew/NumberInput";
+import SliderInput from "../../components/FormComponentsNew/SliderInput";
+import SelectInput from "../../components/FormComponentsNew/SelectInput";
+import DividerForm from "../../components/FormComponentsNew/DividerForm";
+import SubjectInput from "../../components/FormComponents/SubjectInput";
+import CheckboxInput from "../../components/FormComponents/CheckboxInput";
+import TableInput from "../../components/FormComponentsNew/TableInput";
 
-
-const { Title, Paragraph, Text } = Typography
+const { Title, Paragraph, Text } = Typography;
 export default function NewClaim() {
-    const [open, setOpen] = useState(false);
-    const [formValue, setFormValue] = useState(false);
-    const claim = useNewClaim(state => state.claim)
-    const fetchClaim = useNewClaim(state => state.fetchClaim)
-    const createClaim = useNewClaim(state => state.createClaim)
-    const newClaim = useNewClaim(state => state.newClaim)
-    const { id } = useParams()
-    const [form] = Form.useForm()
+  const [open, setOpen] = useState(false);
+  const [formValue, setFormValue] = useState(false);
+  const claim = useNewClaim((state) => state.claim);
+  const fetchClaim = useNewClaim((state) => state.fetchClaim);
+  const createClaim = useNewClaim((state) => state.createClaim);
+  const newClaim = useNewClaim((state) => state.newClaim);
+  const { id } = useParams();
+  const [form] = Form.useForm();
 
-    useEffect(() => {
-        fetchClaim(id)
-    }, [])
-    const showDrawer = () => {
-        setOpen(true);
-    };
-    const onClose = () => {
-        setOpen(false);
-    };
-    //console.log(claim)
-    const onFinish = (values) => {
-        console.log(values)
+  useEffect(() => {
+    fetchClaim(id);
+  }, []);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  //console.log(claim)
+  const onFinish = (values) => {
+    console.log(values);
 
-        setFormValue(values)
-        createClaim({ service: claim.Ref_Key, values })
-        showDrawer()
-    }
-    console.log(claim)
-    return (
-        <div>
-            {claim &&
-                <>
-                    <Title>
-                        {/* <span style={{ color: "gray" }}>Услуга:</span><br />  */}
-                        {claim.Description}
-                    </Title>
-                    <Form
-                        form={form}
-                        layout="vertical"
-                        onFinish={onFinish}
-                        style={{ maxWidth: 800, margin: "0 auto" }}
-                    >
-                        {claim.Fields?.sort((a, b) => a.lineNum - b.lineNum).map((item, index) => {
-                            // console.log(item)
-                            if (item.component_Type.includes("ComponentsDivider"))
-                                return <DividerForm key={index} {...item.component_Expanded} label={item.label} />
-                            if (item.component_Type.includes("ComponentsTextInput"))
-                                return <TextInput key={index} label={item.label} {...item.component_Expanded} {...item} name={item.name_Key} dependOf={item.dependName?.Ref_Key} howDepend={item.dependСondition} />
-                            if (item.component_Type.includes("ComponentsNumberInput"))
-                                return <NumberInput key={index} label={item.label} {...item.component_Expanded} {...item} name={item.name_Key} dependOf={item.dependName?.Ref_Key} howDepend={item.dependСondition} />
-                            if (item.component_Type.includes("ComponentsSliderInput"))
-                                return <SliderInput key={index} label={item.label} {...item.component_Expanded} {...item} name={item.name_Key} dependOf={item.dependName?.Ref_Key} howDepend={item.dependСondition} />
-                            if (item.component_Type.includes("ComponentsLinkInput"))
-                                return <SelectInput key={index} label={item.label} {...item.component_Expanded} {...item} name={item.name_Key} dependOf={item.dependName?.Ref_Key} howDepend={item.dependСondition} />
-                            if (item.component_Type.includes("ComponentsTableInput"))
-                                return <TableInput key={index} label={item.label} {...item.component_Expanded} {...item} name={item.component_Expanded.nameTable_Key} dependOf={item.dependName?.Ref_Key} howDepend={item.dependСondition} />
+    setFormValue(values);
+    createClaim({ service: claim.Ref_Key, values });
+    showDrawer();
+  };
+  console.log(claim);
+  return (
+    <div>
+      {claim && (
+        <>
+          <Title>
+            {/* <span style={{ color: "gray" }}>Услуга:</span><br />  */}
+            {claim.Description}
+          </Title>
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+            style={{ maxWidth: 800, margin: "0 auto" }}
+          >
+            {claim.Fields?.sort((a, b) => a.lineNum - b.lineNum).map(
+              (item, index) => {
+                // console.log(item)
+                if (item.component_Type.includes("ComponentsDivider"))
+                  return (
+                    <DividerForm
+                      key={index}
+                      {...item.component_Expanded}
+                      label={item.label}
+                    />
+                  );
+                if (item.component_Type.includes("ComponentsTextInput"))
+                  return (
+                    <TextInput
+                      key={index}
+                      label={item.label}
+                      {...item.component_Expanded}
+                      {...item}
+                      name={item.name_Key}
+                      dependOf={item.dependName?.Ref_Key}
+                      howDepend={item.dependСondition}
+                    />
+                  );
+                if (item.component_Type.includes("ComponentsNumberInput"))
+                  return (
+                    <NumberInput
+                      key={index}
+                      label={item.label}
+                      {...item.component_Expanded}
+                      {...item}
+                      name={item.name_Key}
+                      dependOf={item.dependName?.Ref_Key}
+                      howDepend={item.dependСondition}
+                    />
+                  );
+                if (item.component_Type.includes("ComponentsSliderInput"))
+                  return (
+                    <SliderInput
+                      key={index}
+                      label={item.label}
+                      {...item.component_Expanded}
+                      {...item}
+                      name={item.name_Key}
+                      dependOf={item.dependName?.Ref_Key}
+                      howDepend={item.dependСondition}
+                    />
+                  );
+                if (item.component_Type.includes("ComponentsLinkInput"))
+                  return (
+                    <SelectInput
+                      key={index}
+                      label={item.label}
+                      {...item.component_Expanded}
+                      {...item}
+                      name={item.name_Key}
+                      dependOf={item.dependName?.Ref_Key}
+                      howDepend={item.dependСondition}
+                    />
+                  );
+                if (item.component_Type.includes("ComponentsTableInput"))
+                  return (
+                    <TableInput
+                      key={index}
+                      label={item.label}
+                      {...item.component_Expanded}
+                      {...item}
+                      name={item.component_Expanded.nameTable_Key}
+                      dependOf={item.dependName?.Ref_Key}
+                      howDepend={item.dependСondition}
+                    />
+                  );
+              }
+            )}
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Подать заявку на услугу
+              </Button>
+            </Form.Item>
+          </Form>
 
-                        })}
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit">Подать заявку на услугу</Button>
-                        </Form.Item>
-                    </Form>
-
-                    <Drawer
-                        title="Поля формы"
-                        placement="bottom"
-                        closable={false}
-                        onClose={onClose}
-                        open={open}
-                        key="bottom"
-                    >
-                        {Object.keys(formValue).map(item => {
-                            // console.log(item)
-                        })}
-                        {/* <Descriptions
+          <Drawer
+            title="Поля формы"
+            placement="bottom"
+            closable={false}
+            onClose={onClose}
+            open={open}
+            key="bottom"
+          >
+            {Object.keys(formValue).map((item) => {
+              // console.log(item)
+            })}
+            {/* <Descriptions
                             bordered
                             title="Данные для отправки в 1С"
                             items={Object.keys(formValue)
@@ -97,11 +155,11 @@ export default function NewClaim() {
                                     label: item,
                                     children: formValue[item],
                                 }))} /> */}
-                        {/* <Paragraph><pre>{JSON.stringify(formValue)}</pre></Paragraph> */}
-                        <Paragraph>Данные вывелись в консоле</Paragraph>
-                    </Drawer>
-                </>
-            }
-        </div>
-    )
+            {/* <Paragraph><pre>{JSON.stringify(formValue)}</pre></Paragraph> */}
+            <Paragraph>Данные вывелись в консоле</Paragraph>
+          </Drawer>
+        </>
+      )}
+    </div>
+  );
 }
