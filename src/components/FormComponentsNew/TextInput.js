28b8fc9cb8fa0c0384a4e05315315006870aaf1a
Stanislav <yanut995@gmail.com>
2024-07-25 15:15:17 +0300

    debounce

--- src/components/FormComponentsNew/TextInput.js
+++ src/components/FormComponentsNew/TextInput.js
@@ -1,5 +1,6 @@
 import { Form, theme, Input, AutoComplete } from "antd";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { debounce } from "lodash";
 import axios from "axios";
 const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
 
@@ -21,26 +22,37 @@ export default function TextInput({
   const form = Form.useFormInstance();
   const fieldDepends = Form.useWatch(dependOf, form);
 
-  useEffect(() => {
-    if (["Фамилия", "Имя", "Отчество", "ИНН"].includes(type) && value) {
-      const fetchSuggestions = async () => {
-        try {
-          const response = await axios.get(`${backServer}/getDaData`, {
-            params: {
-              type,
-              query: value,
-            },
-          });
-          setSuggestions(
-            response.data.data.map((suggestion) => suggestion.value)
-          );
-        } catch (error) {
-          console.error("Error fetching suggestions:", error);
-        }
-      };
+  const fetchSuggestions = async (searchText) => {
+    if (searchText) {
+      try {
+        const response = await axios.get(`${backServer}/getDaData`, {
+          params: {
+            type,
+            query: searchText,
+          },
+        });
+        console.log("DaData response:", response.data);
+        setSuggestions(
+          response.data.data.map((suggestion) => suggestion.value)
+        );
+      } catch (error) {
+        console.error("Error fetching suggestions:", error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
 
-      const debounceFetch = setTimeout(fetchSuggestions, 300);
-      return () => clearTimeout(debounceFetch);
+  const debouncedFetchSuggestions = useCallback(
+    debounce(fetchSuggestions, 800),
+    []
+  );
+
+  useEffect(() => {
+    if (
+      ["Фамилия", "Имя", "Отчество", "ИНН", "Регион", "Город"].includes(type)
+    ) {
+      debouncedFetchSuggestions(value);
     } else {
       setSuggestions([]);
     }
@@ -97,7 +109,8 @@ export default function TextInput({
         "Тип адреса",
         "Удостоверяющий документ",
         "Улица",
-        "Электронный адрес"
+        "Электронный адрес",
+        "Город",
       ].includes(type) ? (
         <AutoComplete
           options={suggestions.map((suggestion) => ({ value: suggestion }))}
@@ -133,7 +146,7 @@ export default function TextInput({
 // import { Form, theme, Input, AutoComplete } from "antd";
 // import { useState, useEffect } from "react";
 // import axios from "axios";
-// const backServer = process.env.REACT_APP_BACK_BACK_SERVER
+// const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
 
 // export default function TextInput({
 //   name = "name",
@@ -154,12 +167,12 @@ export default function TextInput({
 //   const fieldDepends = Form.useWatch(dependOf, form);
 
 //   useEffect(() => {
-//     if (type === "Фамилия" && value) {
+//     if (["Фамилия", "Имя", "Отчество", "ИНН"].includes(type) && value) {
 //       const fetchSuggestions = async () => {
 //         try {
-//           const response = await axios.get(`${backServer}${'/getDaData'}`, {
+//           const response = await axios.get(`${backServer}/getDaData`, {
 //             params: {
-//               type: "Фамилия",
+//               type,
 //               query: value,
 //             },
 //           });
@@ -193,8 +206,44 @@ export default function TextInput({
 //         },
 //       ]}
 //     >
-//       {/* Проверка типа и использование AutoComplete для отображения подсказок */}
-//       {type === "Фамилия" ? (
+//       {[
+//         "Фамилия",
+//         "Имя",
+//         "Отчество",
+//         "ИНН",
+//         "Адрес",
+//         "Банк - наименование",
+//         "БИК",
+//         "Дата выдачи документа",
+//         "Документ",
+//         "Индекс",
+//         "Квартира",
+//         "Кем выдан документа",
+//         "Код кладр",
+//         "Код подразделения документа",
+//         "Код ФИАС",
+//         "Корреспондентский счет",
+//         "КПП",
+//         "Местность",
+//         "Номенклатура",
+//         "Номер документа",
+//         "Номер корпуса",
+//         "Номер строения",
+//         "Полное имя",
+//         "Префикс имени",
+//         "Район",
+//         "Расчетный счет",
+//         "Регион",
+//         "Серия документа",
+//         "СНИЛС",
+//         "Страна",
+//         "Суффикс имени",
+//         "Телефон",
+//         "Тип адреса",
+//         "Удостоверяющий документ",
+//         "Улица",
+//         "Электронный адрес"
+//       ].includes(type) ? (
 //         <AutoComplete
 //           options={suggestions.map((suggestion) => ({ value: suggestion }))}
 //           onChange={handlerOnChange}
@@ -222,83 +271,6 @@ export default function TextInput({
 //     if (show) return formElement;
 //   }
 //   if (dependOf && howDepend && howDepend.min && howDepend.max) {
-//     if (fieldDepends >= howDepend.min && howDepend <= howDepend.max)
-//       return formElement;
-//   }
-// }
-
-// import { Form, theme, Input } from "antd";
-// import { MaskedInput } from "antd-mask-input";
-// import { useState } from "react";
-
-// export default function TextInput({
-//   name = "name",
-//   label = "Label",
-//   disabled = false,
-//   placeholder = "Пример",
-//   required = false,
-//   dependOf = false,
-//   howDepend = false,
-//   inputMask = false,
-//   lenght = false,
-//   type=false
-// }) {
-//   const { token } = theme.useToken();
-//   const [value, setValue] = useState();
-//   const form = Form.useFormInstance();
-//   const fieldDepends = Form.useWatch(dependOf, form);
-//   // console.log(name)
-//   const handlerOnChange = (e) => {
-//     console.log(e.target.value);
-//     // setValue(e.target.value)
-//     // form.setFieldValue(name, e.target.value)
-//   };
-//   // console.log(lenght)
-//   const formElement = (
-//     <Form.Item
-//       name={name}
-//       label={label}
-//       rules={[
-//         {
-//           required: required,
-//           message: "Это поле обязательное",
-//         },
-//       ]}
-//     // valuePropName={name} colorPrimaryHover
-//     >
-//       {/* <ReactInputMask className='ant-input css-dev-only-do-not-override-1sbryic ant-input-outlined ant-input-status-success' onChange={handlerOnChange} /> */}
-//       <Input placeholder={placeholder} maxLength={lenght} />
-//       {/* <MaskedInput
-//         maskOptions={{
-//           mask: inputMask == "" ? false : inputMask,
-//           overwrite: "shift",
-//         }}
-//         maxLength={lenght == 0 ? false : lenght}
-//         style={{
-//           backgroundColor: token.colorBgContainer,
-//           //  borderColor: token.colorBorder
-//         }}
-//         className={"ant-picker-outlined"}
-//       /> */}
-//       {/* <ReactInputMask mask={mask} disabled={disabled} onChange={handlerOnChange} >
-//                 {(inputProps) => {
-//                     console.log(inputProps)
-//                     return <Input {...inputProps} disabled={disabled} placeholder={placeholder} />
-//                 }}
-//             </ReactInputMask> */}
-//     </Form.Item>
-//   );
-//   if (!dependOf) return formElement;
-//   if (dependOf && howDepend && howDepend.values.length > 0) {
-//     let show = false;
-//     howDepend.values.forEach((item) => {
-//       if (item.value === "true") item.value = true;
-//       if (item.value == fieldDepends) show = true;
-//     });
-//     if (show) return formElement;
-//   }
-//   if (dependOf && howDepend && howDepend.min && howDepend.max) {
-//     if (fieldDepends >= howDepend.min && fieldDepends <= howDepend.max)
-//       return formElement;
+//     if (fieldDepends >= howDepend.min && howDepend.max) return formElement;
 //   }
 // }
