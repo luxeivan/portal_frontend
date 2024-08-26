b27f85dd1725fe9d1c32fc70c6460b1d1539997a
luxeivan <luxeivan@gmail.com>
2024-08-26 15:25:27 +0300

    commit & push

--- src/components/Documentation/Law.js
+++ src/components/Documentation/Law.js
@@ -19,45 +19,46 @@ const type = {
 const siteMosoblServer = process.env.REACT_APP_BACK_SITE_MOSOBLENERGO_SERVER
 
 export default function RegulatoryLegalActs() {
-    const [docs, setDocs] = useState([])
-    useEffect(() => {
-      axios.get(`${siteMosoblServer}/api/tp-normativno-pravovye-akty?populate=*`)
-        .then(response => {
-          if (response.data) {
-            // console.log(response.data.data.attributes.docs.data)
-            setDocs(response.data.data.attributes.docs.data)
-            // setDocs()
-          }
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }, [])
-    return (
-      <div className="page-grid__content">
-        <div className="row-docs-age">
-          {docs.length > 0 &&
-            docs.map((item, index) =>
-              <a
-                className={styles.docLine}
-                href={`${siteMosoblServer}${item.attributes.url}`}
-                download=""
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <div className={styles.docLine__wrapIcon}>
-                  <img
-                    src={type[item.attributes.ext.slice(1)]}
-                    alt={`icon ${item.attributes.ext.slice(1)}`}
-                  />
-                </div>
-                <div className="docLine__wrapText">
-                  <span className={styles.docLine__name}>{item.attributes.name}</span>
-                  <span className={styles.docLine__fileInfo}>{Number(item.attributes.size) > 1000 ? `${(item.attributes.size / 1000).toFixed(2)} МБ`:`${Math.round(item.attributes.size)} КБ`}</span>
-                </div>
-              </a>
-            )}        
-        </div>
+  const [docs, setDocs] = useState([])
+  useEffect(() => {
+    axios.get(`${siteMosoblServer}/api/tp-normativno-pravovye-akty?populate=*`)
+      .then(response => {
+        if (response.data) {
+          // console.log(response.data.data.attributes.docs.data)
+          setDocs(response.data.data.attributes.docs.data)
+          // setDocs()
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+  return (
+    <div className="page-grid__content">
+      <div className="row-docs-age">
+        {docs.length > 0 &&
+          docs.map((item, index) =>
+            <a
+              key={index}
+              className={styles.docLine}
+              href={`${siteMosoblServer}${item.attributes.url}`}
+              download=""
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <div className={styles.docLine__wrapIcon}>
+                <img
+                  src={type[item.attributes.ext.slice(1)]}
+                  alt={`icon ${item.attributes.ext.slice(1)}`}
+                />
+              </div>
+              <div className="docLine__wrapText">
+                <span className={styles.docLine__name}>{item.attributes.name}</span>
+                <span className={styles.docLine__fileInfo}>{Number(item.attributes.size) > 1000 ? `${(item.attributes.size / 1000).toFixed(2)} МБ` : `${Math.round(item.attributes.size)} КБ`}</span>
+              </div>
+            </a>
+          )}
       </div>
-    );
+    </div>
+  );
 }
