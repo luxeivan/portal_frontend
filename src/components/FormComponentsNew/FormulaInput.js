f08715730cf6409d181748867c6ecb8326474535
luxeivan <luxeivan@gmail.com>
2024-09-06 22:03:58 +0300

    добавил модуль mathjs

--- src/components/FormComponentsNew/FormulaInput.js
+++ src/components/FormComponentsNew/FormulaInput.js
@@ -5,6 +5,7 @@ import {
     Input,
     theme
 } from "antd";
+import { evaluate } from "mathjs";
 
 export default function FormulaInput({
     name = "name",
@@ -18,23 +19,53 @@ export default function FormulaInput({
     max = 100,
     step = 1,
     defaultValue = false,
-    properties = false
+    properties = false,
+    formula = ''
 }) {
+    // const [propertiesValue, setPropertiesValue] = useState({})
     // "{"ТипЦены": "f6e1ac07-8fab-49e2-9d34-f859a2a8dcf8","Номенклатура": "2406f62a-2998-4578-9fa2-b2582dcc7a26"}"
     const { colorTextHeading } = theme.useToken().token
     const form = Form.useFormInstance();
-    let objectProp = null
+    let objectProp = {}
     if (properties) objectProp = JSON.parse(properties)
-    let count = 0
-    let price = 0
-    count = Form.useWatch(objectProp["Количество"], form);
-    price = Form.useWatch(objectProp["Цена"], form);
-    useEffect(() => {
-        form.setFieldValue(name, count * price)
-    }, [count, price])
+    let keys = []
+    for (let key in objectProp) {
+        if (objectProp.hasOwnProperty(key)) {
+            keys.push(objectProp[key]);
+        }
+    }
+    console.log(properties)
+
+    // console.log(keys.map(item => ([item])))
+    Form.useWatch((values) => {
+        const temp = {}
+        keys.forEach(item => {
+            if (!values[item]) return;
+            temp[item] = values[item]
+        })
+        console.log("evaluate",evaluate(formula, temp))
+        if (values['720c36c9-4eb7-4fa8-a970-28c65bc3edba'] && values['8a381dcc-33bd-4cbc-8f42-03297ceacf40'] && values['720c36c9-4eb7-4fa8-a970-28c65bc3edba'] * values['8a381dcc-33bd-4cbc-8f42-03297ceacf40'] !== values[name]) {
+            form.setFieldValue(name, values['720c36c9-4eb7-4fa8-a970-28c65bc3edba'] * values['8a381dcc-33bd-4cbc-8f42-03297ceacf40'])
+        }
+        // setPropertiesValue(temp)
+        // console.log("values: ", values)
+        // console.log("temp: ", temp)
+        // console.log("name: ", name)
+    }, form);
+    // const propertiesValue = Form.useWatch(['8a381dcc-33bd-4cbc-8f42-03297ceacf40'], form)
+
+    // let count = 0
+    // let price = 0
+    // count = Form.useWatch(objectProp["Количество"], form);
+    // price = Form.useWatch(objectProp["Цена"], form);
+    // console.log(propertiesValue)
+    // useEffect(() => {
+    //     form.setFieldValue(name, 1 * 1)
+    // }, [propertiesValue])
     let fieldDepends = Form.useWatch(dependOf, form);
     // console.log('defaultValue',defaultValue)
     // console.log('propsFormula', objectProp)
+    if (formula === '') return false;
     const formElement = (
         <Form.Item
             name={name}
