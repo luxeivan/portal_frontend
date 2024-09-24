57aaa20935cb2636739d96ee32e6de5d4c75b24b
Stanislav <yanut995@gmail.com>
2024-09-24 12:00:47 +0300

    Комитыч

--- src/components/Global/AppFooter.js
+++ src/components/Global/AppFooter.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Layout, Space } from "antd";
-import styles from "./AppFooter.module.css";
+import { Layout, Space, Typography, Button, Row, Col } from "antd";
 
 // Импортируем локальные изображения
 import telegaIcon from "../../img/socialMedia/telega.png";
@@ -9,76 +8,173 @@ import okIcon from "../../img/socialMedia/ok.png";
 import yandexIcon from "../../img/socialMedia/yandex.png";
 
 const { Footer } = Layout;
+const { Link } = Typography;
 
 export default function AppFooter() {
   return (
     <Footer
-      className={styles.footer}
-      // style={{
-      //   textAlign: "center",
-      //   padding: "20px 0", // Добавляем немного паддинга
-      // }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        width: "100%",
+        // padding: "20px 0",
+        // backgroundColor: "#f0f2f5", 
+      }}
     >
-      <Space direction="vertical" size="large">
+      <Space direction="vertical" size="large" align="center">
         {/* Социальные сети */}
-        <div>
-          <Space size="large">
-            <a
+        <Row gutter={[16, 16]} justify="center">
+          <Col>
+            <Button
+              type="link"
               href="https://t.me/mosoblenergo"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <img
-                src={telegaIcon}
-                alt="Telegram"
-                style={{ width: 40, height: 40 }}
-              />
-            </a>
-            <a
+              icon={
+                <img
+                  src={telegaIcon}
+                  alt="Telegram"
+                  style={{ width: 30, height: 30 }}
+                />
+              }
+            />
+          </Col>
+          <Col>
+            <Button
+              type="link"
               href="https://vk.com/mosoblenergo"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <img src={vkIcon} alt="VK" style={{ width: 40, height: 40 }} />
-            </a>
-            <a
+              icon={
+                <img src={vkIcon} alt="VK" style={{ width: 30, height: 30 }} />
+              }
+            />
+          </Col>
+          <Col>
+            <Button
+              type="link"
               href="https://ok.ru/mosoblenergo"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <img src={okIcon} alt="OK" style={{ width: 40, height: 40 }} />
-            </a>
-            <a
+              icon={
+                <img src={okIcon} alt="OK" style={{ width: 30, height: 30 }} />
+              }
+            />
+          </Col>
+          <Col>
+            <Button
+              type="link"
               href="https://dzen.ru/mosoblenergo"
               target="_blank"
               rel="noopener noreferrer"
-            >
-              <img
-                src={yandexIcon}
-                alt="Yandex"
-                style={{ width: 40, height: 40 }}
-              />
-            </a>
-          </Space>
-        </div>
+              icon={
+                <img
+                  src={yandexIcon}
+                  alt="Yandex"
+                  style={{ width: 30, height: 30 }}
+                />
+              }
+            />
+          </Col>
+        </Row>
 
         {/* Текущая информация о сайте */}
-        <div>
+        <Typography.Text>
           Copyright ©{" "}
-          <a
+          <Link
             href="https://mosoblenergo.ru/"
             target="_blank"
             rel="noopener noreferrer"
           >
             АО «Мособлэнерго»
-          </a>{" "}
+          </Link>{" "}
           | Разработка сайта - Шишкин & Януть | 2023 -{" "}
           {new Date().getFullYear()}
-        </div>
+        </Typography.Text>
       </Space>
     </Footer>
   );
 }
+
+// import React from "react";
+// import { Layout, Space } from "antd";
+// import styles from "./AppFooter.module.css";
+
+// // Импортируем локальные изображения
+// import telegaIcon from "../../img/socialMedia/telega.png";
+// import vkIcon from "../../img/socialMedia/vk.png";
+// import okIcon from "../../img/socialMedia/ok.png";
+// import yandexIcon from "../../img/socialMedia/yandex.png";
+
+// const { Footer } = Layout;
+
+// export default function AppFooter() {
+//   return (
+//     <Footer
+//       className={styles.footer}
+//     >
+//       <Space direction="vertical" size="large">
+//         {/* Социальные сети */}
+//         <div>
+//           <Space size="large">
+//             <a
+//               href="https://t.me/mosoblenergo"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img
+//                 src={telegaIcon}
+//                 alt="Telegram"
+//                 style={{ width: 30, height: 30 }}
+//               />
+//             </a>
+//             <a
+//               href="https://vk.com/mosoblenergo"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img src={vkIcon} alt="VK" style={{ width: 30, height: 30 }} />
+//             </a>
+//             <a
+//               href="https://ok.ru/mosoblenergo"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img src={okIcon} alt="OK" style={{ width: 30, height: 30 }} />
+//             </a>
+//             <a
+//               href="https://dzen.ru/mosoblenergo"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img
+//                 src={yandexIcon}
+//                 alt="Yandex"
+//                 style={{ width: 30, height: 30 }}
+//               />
+//             </a>
+//           </Space>
+//         </div>
+
+//         {/* Текущая информация о сайте */}
+//         <div>
+//           Copyright ©{" "}
+//           <a
+//             href="https://mosoblenergo.ru/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             АО «Мособлэнерго»
+//           </a>{" "}
+//           | Разработка сайта - Шишкин & Януть | 2023 -{" "}
+//           {new Date().getFullYear()}
+//         </div>
+//       </Space>
+//     </Footer>
+//   );
+// }
 
 // import React from "react";
 // import { Layout } from "antd";
