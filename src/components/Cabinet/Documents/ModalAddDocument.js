d936ce2f94afe22228ef44d4b02c7a48df686527
Stanislav <yanut995@gmail.com>
2024-11-12 15:55:59 +0300

    Фикс фиксиков

--- src/components/Cabinet/Documents/ModalAddDocument.js
+++ src/components/Cabinet/Documents/ModalAddDocument.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import useDocuments from "../../../stores/Cabinet/useDocuments";
 import UploaderInput from "../../FormComponents/UploaderInput";
 import ErrorModal from "../../ErrorModal";
-import Preloader from "../../Main/Preloader"; // Импортируем ваш прелоадер
-import styles from "./ModalAddDocument.module.css"; // Создайте CSS-модуль для стилей
+import Preloader from "../../Main/Preloader";
+import styles from "./ModalAddDocument.module.css";
 
 const { Option } = Select;
 const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
@@ -26,11 +26,10 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
   const [categoriesData, setCategoriesData] = useState([]);
   const [categories, setCategories] = useState([]);
   const [allowedExtensions, setAllowedExtensions] = useState([]);
-  const [maxFileSize, setMaxFileSize] = useState(10); // По умолчанию 10 МБ
+  const [maxFileSize, setMaxFileSize] = useState(10);
   const [isCategoryDisabled, setIsCategoryDisabled] = useState(false);
 
   useEffect(() => {
-    // Получение категорий документов из бэкенда
     const fetchCategories = async () => {
       try {
         const response = await axios.get(
@@ -43,7 +42,6 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
           }
         );
         setCategoriesData(response.data.categories);
-        // Извлекаем уникальные категории
         const uniqueCategories = [
           ...new Set(
             response.data.categories.map((item) => item.category.Description)
@@ -59,7 +57,7 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
   }, [token]);
 
   useEffect(() => {
-    if (categoryKey && categoriesData.length > 0) {
+    if (categoryKey && categoriesData.length > 0 && visible) {
       const selectedCategoryItem = categoriesData.find(
         (item) => item.category.Ref_Key === categoryKey
       );
@@ -70,18 +68,21 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
           documentName: selectedCategory,
           categoryKey: selectedCategoryItem.category.Ref_Key,
         });
-        setIsCategoryDisabled(true); // Блокируем поле 'category'
-        // Устанавливаем допустимые расширения и максимальный размер файла
+        setIsCategoryDisabled(true);
         const extensions = JSON.parse(
           selectedCategoryItem.category.availableExtensionsJSON
         );
         setAllowedExtensions(extensions);
         setMaxFileSize(parseInt(selectedCategoryItem.category.maximumSize));
       }
-    } else {
-      setIsCategoryDisabled(false); // Разблокируем поле 'category'
+    } else if (!visible) {
+      form.resetFields();
+      setAllowedExtensions([]);
+      setMaxFileSize(10);
+      setUploaderKey((prevKey) => prevKey + 1);
+      setIsCategoryDisabled(false);
     }
-  }, [categoryKey, categoriesData, form]);
+  }, [categoryKey, categoriesData, form, visible]);
 
   const handleModalClose = () => {
     if (onClose) {
@@ -89,11 +90,6 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
     } else {
       setOpenModalAdd(false);
     }
-    form.resetFields();
-    setAllowedExtensions([]);
-    setMaxFileSize(10);
-    setUploaderKey((prevKey) => prevKey + 1);
-    setIsCategoryDisabled(false); // Сбрасываем блокировку поля 'category'
   };
 
   const handleSaveDocument = async (values) => {
@@ -153,7 +149,6 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
         destroyOnClose={true}
       >
         <Form form={form} onFinish={handleSaveDocument}>
-          {/* Категория */}
           <Form.Item
             label="Категория"
             name="category"
@@ -196,7 +191,6 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
             </Select>
           </Form.Item>
 
-          {/* Название */}
           <Form.Item
             noStyle
             shouldUpdate={(prevValues, currentValues) =>
@@ -235,7 +229,6 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
             }}
           </Form.Item>
 
-          {/* UploaderInput и кнопка сохранения */}
           <Form.Item
             noStyle
             shouldUpdate={(prevValues, currentValues) =>
@@ -282,6 +275,291 @@ export default function ModalAddDocument({ visible, onClose, categoryKey }) {
     </>
   );
 }
+
+// import React, { useState, useEffect } from "react";
+// import { Modal, Button, message, Form, Select, Input } from "antd";
+// import axios from "axios";
+// import useDocuments from "../../../stores/Cabinet/useDocuments";
+// import UploaderInput from "../../FormComponents/UploaderInput";
+// import ErrorModal from "../../ErrorModal";
+// import Preloader from "../../Main/Preloader"; // Импортируем ваш прелоадер
+// import styles from "./ModalAddDocument.module.css"; // Создайте CSS-модуль для стилей
+
+// const { Option } = Select;
+// const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
+
+// export default function ModalAddDocument({ visible, onClose, categoryKey }) {
+//   const openModalAdd = useDocuments((state) => state.openModalAdd);
+//   const setOpenModalAdd = useDocuments((state) => state.setOpenModalAdd);
+//   const fetchDocuments = useDocuments((state) => state.fetchDocuments);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
+//   const [errorVisible, setErrorVisible] = useState(false);
+//   const [uploaderKey, setUploaderKey] = useState(0);
+
+//   const [form] = Form.useForm();
+
+//   const token = localStorage.getItem("jwt");
+
+//   const [categoriesData, setCategoriesData] = useState([]);
+//   const [categories, setCategories] = useState([]);
+//   const [allowedExtensions, setAllowedExtensions] = useState([]);
+//   const [maxFileSize, setMaxFileSize] = useState(10); // По умолчанию 10 МБ
+//   const [isCategoryDisabled, setIsCategoryDisabled] = useState(false);
+
+//   useEffect(() => {
+//     // Получение категорий документов из бэкенда
+//     const fetchCategories = async () => {
+//       try {
+//         const response = await axios.get(
+//           `${backServer}/api/cabinet/documents/categories`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//             withCredentials: true,
+//           }
+//         );
+//         setCategoriesData(response.data.categories);
+//         // Извлекаем уникальные категории
+//         const uniqueCategories = [
+//           ...new Set(
+//             response.data.categories.map((item) => item.category.Description)
+//           ),
+//         ];
+//         setCategories(uniqueCategories);
+//       } catch (error) {
+//         console.error("Ошибка при загрузке категорий документов:", error);
+//       }
+//     };
+
+//     fetchCategories();
+//   }, [token]);
+
+//   useEffect(() => {
+//     if (categoryKey && categoriesData.length > 0) {
+//       const selectedCategoryItem = categoriesData.find(
+//         (item) => item.category.Ref_Key === categoryKey
+//       );
+//       if (selectedCategoryItem) {
+//         const selectedCategory = selectedCategoryItem.category.Description;
+//         form.setFieldsValue({
+//           category: selectedCategory,
+//           documentName: selectedCategory,
+//           categoryKey: selectedCategoryItem.category.Ref_Key,
+//         });
+//         setIsCategoryDisabled(true); // Блокируем поле 'category'
+//         // Устанавливаем допустимые расширения и максимальный размер файла
+//         const extensions = JSON.parse(
+//           selectedCategoryItem.category.availableExtensionsJSON
+//         );
+//         setAllowedExtensions(extensions);
+//         setMaxFileSize(parseInt(selectedCategoryItem.category.maximumSize));
+//       }
+//     } else {
+//       setIsCategoryDisabled(false); // Разблокируем поле 'category'
+//     }
+//   }, [categoryKey, categoriesData, form]);
+
+//   const handleModalClose = () => {
+//     if (onClose) {
+//       onClose();
+//     } else {
+//       setOpenModalAdd(false);
+//     }
+//     form.resetFields();
+//     setAllowedExtensions([]);
+//     setMaxFileSize(10);
+//     setUploaderKey((prevKey) => prevKey + 1);
+//     setIsCategoryDisabled(false); // Сбрасываем блокировку поля 'category'
+//   };
+
+//   const handleSaveDocument = async (values) => {
+//     try {
+//       setLoading(true);
+//       const files = form.getFieldValue("fileDoc");
+
+//       if (!files || files.length === 0) {
+//         message.error("Пожалуйста, загрузите файлы");
+//         setLoading(false);
+//         return;
+//       }
+
+//       const formData = new FormData();
+//       formData.append("category", values.category);
+//       formData.append("documentName", values.documentName);
+//       formData.append("categoryKey", values.categoryKey);
+
+//       files.forEach((file) => {
+//         formData.append("files", file.originFileObj);
+//       });
+
+//       const token = localStorage.getItem("jwt");
+
+//       await axios.post(`${backServer}/api/cabinet/upload-file`, formData, {
+//         headers: {
+//           "Content-Type": "multipart/form-data",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         withCredentials: true,
+//       });
+
+//       message.success("Документ успешно сохранен");
+//       fetchDocuments(categoryKey);
+//       handleModalClose();
+//       setUploaderKey((prevKey) => prevKey + 1);
+//     } catch (error) {
+//       console.error("Ошибка при сохранении документа:", error);
+//       setError(error.message || "Неизвестная ошибка");
+//       setErrorVisible(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   const closeModal = () => {
+//     setErrorVisible(false);
+//   };
+
+//   return (
+//     <>
+//       <Modal
+//         title="Загрузить новый документ"
+//         open={openModalAdd || visible}
+//         onCancel={handleModalClose}
+//         footer={null}
+//         destroyOnClose={true}
+//       >
+//         <Form form={form} onFinish={handleSaveDocument}>
+//           {/* Категория */}
+//           <Form.Item
+//             label="Категория"
+//             name="category"
+//             rules={[
+//               {
+//                 required: true,
+//                 message: "Пожалуйста, выберите категорию документа",
+//               },
+//             ]}
+//           >
+//             <Select
+//               placeholder="Выберите категорию"
+//               disabled={isCategoryDisabled}
+//               onChange={(value) => {
+//                 const selectedCategoryItem = categoriesData.find(
+//                   (item) => item.category.Description === value
+//                 );
+//                 if (selectedCategoryItem) {
+//                   const selectedCategory =
+//                     selectedCategoryItem.category.Description;
+//                   form.setFieldsValue({
+//                     documentName: selectedCategory,
+//                     categoryKey: selectedCategoryItem.category.Ref_Key,
+//                   });
+//                   const extensions = JSON.parse(
+//                     selectedCategoryItem.category.availableExtensionsJSON
+//                   );
+//                   setAllowedExtensions(extensions);
+//                   setMaxFileSize(
+//                     parseInt(selectedCategoryItem.category.maximumSize)
+//                   );
+//                 }
+//               }}
+//             >
+//               {categories.map((category, index) => (
+//                 <Option key={index} value={category}>
+//                   {category}
+//                 </Option>
+//               ))}
+//             </Select>
+//           </Form.Item>
+
+//           {/* Название */}
+//           <Form.Item
+//             noStyle
+//             shouldUpdate={(prevValues, currentValues) =>
+//               prevValues.category !== currentValues.category ||
+//               prevValues.documentName !== currentValues.documentName
+//             }
+//           >
+//             {() => {
+//               const selectedCategory = form.getFieldValue("category");
+//               const documentName = form.getFieldValue("documentName");
+
+//               return (
+//                 <>
+//                   {(selectedCategory || isCategoryDisabled) && (
+//                     <>
+//                       <Form.Item
+//                         label="Название"
+//                         name="documentName"
+//                         rules={[
+//                           {
+//                             required: true,
+//                             message: "Пожалуйста, введите название документа",
+//                           },
+//                         ]}
+//                       >
+//                         <Input />
+//                       </Form.Item>
+
+//                       <Form.Item name="categoryKey" hidden>
+//                         <input type="hidden" />
+//                       </Form.Item>
+//                     </>
+//                   )}
+//                 </>
+//               );
+//             }}
+//           </Form.Item>
+
+//           {/* UploaderInput и кнопка сохранения */}
+//           <Form.Item
+//             noStyle
+//             shouldUpdate={(prevValues, currentValues) =>
+//               prevValues.documentName !== currentValues.documentName
+//             }
+//           >
+//             {() => {
+//               const documentName = form.getFieldValue("documentName");
+
+//               return (
+//                 <>
+//                   {(documentName || isCategoryDisabled) && (
+//                     <>
+//                       <UploaderInput
+//                         resetTrigger={uploaderKey}
+//                         allowedExtensions={allowedExtensions}
+//                         maxFileSize={maxFileSize}
+//                         loading={loading}
+//                       />
+
+//                       <Form.Item>
+//                         <Button
+//                           type="primary"
+//                           htmlType="submit"
+//                           disabled={loading}
+//                         >
+//                           Сохранить файлы
+//                         </Button>
+//                       </Form.Item>
+//                     </>
+//                   )}
+//                 </>
+//               );
+//             }}
+//           </Form.Item>
+//         </Form>
+//         {loading && (
+//           <div className={styles.overlay}>
+//             <Preloader />
+//           </div>
+//         )}
+//         <ErrorModal visible={errorVisible} error={error} onClose={closeModal} />
+//       </Modal>
+//     </>
+//   );
+// }
 
 // import React, { useState, useEffect } from "react";
 // import { Modal, Button, message, Form, Select, Input } from "antd";
--- src/pages/Cabinet/Documents/Documents.js
+++ src/pages/Cabinet/Documents/Documents.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
-import { Typography, Card, Button, Modal } from "antd";
+import { Typography, Card, Modal } from "antd";
 import AppHelmet from "../../../components/Global/AppHelmet";
-import {
-  PlusOutlined,
-  FilePdfOutlined,
-  DeleteOutlined,
-  EyeOutlined,
-} from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
 import SceletonCard from "../../../components/SceletonCard";
 import useDocuments from "../../../stores/Cabinet/useDocuments";
 import axios from "axios";
 import ModalAddDocument from "../../../components/Cabinet/Documents/ModalAddDocument";
 import DocumentCard from "./DocumentCard";
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 const Documents = ({ categoryKey, onSelectDocument, isModal }) => {
   const [modalCategoryKey, setModalCategoryKey] = useState(null);
@@ -100,6 +95,7 @@ const Documents = ({ categoryKey, onSelectDocument, isModal }) => {
   const handleCloseModal = () => {
     setOpenModalAdd(false);
     setIsModalOpen(false);
+    setModalCategoryKey(null);
   };
 
   const documentCards = useMemo(() => {
@@ -148,6 +144,157 @@ const Documents = ({ categoryKey, onSelectDocument, isModal }) => {
 };
 
 export default React.memo(Documents);
+
+// import React, { useEffect, useState, useMemo, useCallback } from "react";
+// import { Typography, Card, Button, Modal } from "antd";
+// import AppHelmet from "../../../components/Global/AppHelmet";
+// import {
+//   PlusOutlined,
+//   FilePdfOutlined,
+//   DeleteOutlined,
+//   EyeOutlined,
+// } from "@ant-design/icons";
+// import SceletonCard from "../../../components/SceletonCard";
+// import useDocuments from "../../../stores/Cabinet/useDocuments";
+// import axios from "axios";
+// import ModalAddDocument from "../../../components/Cabinet/Documents/ModalAddDocument";
+// import DocumentCard from "./DocumentCard";
+
+// const { Title, Text } = Typography;
+
+// const Documents = ({ categoryKey, onSelectDocument, isModal }) => {
+//   const [modalCategoryKey, setModalCategoryKey] = useState(null);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const documents = useDocuments((state) => state.documents);
+//   const loadingDocuments = useDocuments((state) => state.loadingDocuments);
+//   const openModalAdd = useDocuments((state) => state.openModalAdd);
+//   const setOpenModalAdd = useDocuments((state) => state.setOpenModalAdd);
+//   const fetchDocuments = useDocuments((state) => state.fetchDocuments);
+//   const deleteDocument = useDocuments((state) => state.deleteDocument);
+
+//   useEffect(() => {
+//     fetchDocuments(categoryKey);
+//   }, [categoryKey, fetchDocuments]);
+
+//   const openDocument = useCallback((document) => {
+//     const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
+//     const newWindow = window.open("", "_blank");
+//     let fileUrl;
+
+//     if (document.ПутьКФайлу) {
+//       const fileName = document.ПутьКФайлу.split("/")[1];
+//       fileUrl = `${backServer}/api/cabinet/get-file/by-filename/${fileName}`;
+//     } else {
+//       const fileId = document.Ref_Key;
+//       fileUrl = `${backServer}/api/cabinet/get-file/by-id/${fileId}`;
+//     }
+
+//     axios
+//       .get(fileUrl, {
+//         headers: {
+//           Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+//         },
+//         responseType: "blob",
+//         withCredentials: true,
+//       })
+//       .then((response) => {
+//         const file = new Blob([response.data], { type: "application/pdf" });
+//         const fileURL = URL.createObjectURL(file);
+//         newWindow.location.href = fileURL;
+//       })
+//       .catch((error) => {
+//         console.error("Ошибка при открытии документа:", error);
+//         newWindow.document.write("<p>Не удалось загрузить документ.</p>");
+//       });
+//   }, []);
+
+//   const handleDocumentClick = useCallback(
+//     (document) => {
+//       if (onSelectDocument) {
+//         onSelectDocument(document);
+//       } else {
+//         openDocument(document);
+//       }
+//     },
+//     [onSelectDocument, openDocument]
+//   );
+
+//   const confirmDelete = useCallback(
+//     (id) => {
+//       Modal.confirm({
+//         title: "Вы уверены, что хотите удалить этот документ?",
+//         okText: "Да",
+//         okType: "danger",
+//         cancelText: "Отмена",
+//         onOk() {
+//           deleteDocument(id);
+//         },
+//       });
+//     },
+//     [deleteDocument]
+//   );
+
+//   const handleAddDocument = useCallback(() => {
+//     setOpenModalAdd(true);
+//     setIsModalOpen(true);
+//     if (isModal) {
+//       setModalCategoryKey(categoryKey);
+//     } else {
+//       setModalCategoryKey(null);
+//     }
+//   }, [setOpenModalAdd, isModal, categoryKey]);
+
+//   const handleCloseModal = () => {
+//     setOpenModalAdd(false);
+//     setIsModalOpen(false);
+//   };
+
+//   const documentCards = useMemo(() => {
+//     return documents.map((document, index) => (
+//       <DocumentCard
+//         key={index}
+//         document={document}
+//         isModal={isModal}
+//         handleDocumentClick={handleDocumentClick}
+//         confirmDelete={confirmDelete}
+//         openDocument={openDocument}
+//       />
+//     ));
+//   }, [documents, isModal, handleDocumentClick, confirmDelete, openDocument]);
+
+//   return (
+//     <div>
+//       <AppHelmet title={"Документы"} desc={"Документы"} />
+//       <Title level={1}>Документы</Title>
+//       <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+//         {loadingDocuments && <SceletonCard />}
+//         {documentCards}
+//         <Card
+//           hoverable={!isModalOpen}
+//           style={{
+//             width: 250,
+//             height: 250,
+//             display: "flex",
+//             alignItems: "center",
+//             justifyContent: "center",
+//             opacity: isModalOpen ? 0.5 : 1,
+//             cursor: isModalOpen ? "not-allowed" : "pointer",
+//           }}
+//           onClick={!isModalOpen ? handleAddDocument : undefined}
+//         >
+//           <PlusOutlined style={{ fontSize: "24px" }} />
+//         </Card>
+//       </div>
+//       <ModalAddDocument
+//         visible={openModalAdd}
+//         onClose={handleCloseModal}
+//         categoryKey={modalCategoryKey}
+//       />
+//     </div>
+//   );
+// };
+
+// export default React.memo(Documents);
 
 // import React, { useEffect, useState, useMemo, useCallback } from "react";
 // import { Typography, Card, Modal } from "antd";
