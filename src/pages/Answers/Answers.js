6faa65ae3f99ee3538bdc188aa483b8fd6cd8e27
Stanislav <yanut995@gmail.com>
2024-12-03 09:02:02 +0300

    Поменял Ответики

--- src/pages/Answers/Answers.js
+++ src/pages/Answers/Answers.js
@@ -26,7 +26,7 @@ export default function Answers() {
   }, []);
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px", maxWidth:"1600px" }}>
       <Title level={2}>Часто задаваемые вопросы</Title>
 
       <Card>
@@ -66,66 +66,3 @@ export default function Answers() {
     </div>
   );
 }
-
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Collapse, Button, Typography } from "antd";
-// import { RightOutlined } from "@ant-design/icons";
-// import ModalBot from "../../components/Global/ModalBot";
-
-// const { Text, Title } = Typography;
-
-// const { Panel } = Collapse;
-
-// const backServer = process.env.REACT_APP_BACK_BACK_SERVER;
-
-// export default function Answers() {
-//   const [questions, setQuestions] = useState([]);
-//   const [chatModalVisible, setChatModalVisible] = useState(false);
-
-//   useEffect(() => {
-//     const fetchQuestions = async () => {
-//       try {
-//         const response = await axios.get(`${backServer}/api/hotquestions`);
-//         setQuestions(response.data);
-//       } catch (error) {
-//         console.error("Ошибка при получении вопросов/ответов:", error);
-//       }
-//     };
-//     fetchQuestions();
-//   }, []);
-
-//   return (
-//     <div style={{ padding: "20px" }}>
-//       <Title level={2}>Часто задаваемые вопросы</Title>
-
-//       <Collapse
-//         accordion
-//         expandIcon={({ isActive }) => (
-//           <RightOutlined rotate={isActive ? 90 : 0} />
-//         )}
-//       >
-//         {questions.map((q, index) => (
-//           <Panel header={q.question} key={index}>
-//             <Text>{q.answer}</Text>
-//           </Panel>
-//         ))}
-//       </Collapse>
-
-//       {/* Кнопка для открытия чат-бота */}
-//       <Button
-//         type="primary"
-//         onClick={() => setChatModalVisible(true)}
-//         style={{ marginTop: "20px" }}
-//       >
-//         Задать вопрос
-//       </Button>
-
-//       {/* Модальное окно чат-бота */}
-//       <ModalBot
-//         visible={chatModalVisible}
-//         onClose={() => setChatModalVisible(false)}
-//       />
-//     </div>
-//   );
-// }
