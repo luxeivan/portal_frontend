ebdc665d75a185ed2ee76d6ba0c86b75a5399f72
Stanislav <yanut995@gmail.com>
2024-12-10 10:01:07 +0300

    Тестим новое окошко ввода часть 1

--- src/components/Global/Auth/Login/CodeForm.js
+++ src/components/Global/Auth/Login/CodeForm.js
@@ -38,15 +38,72 @@ export default function CodeForm() {
             { required: true, message: "Пожалуйста, введите код из СМС" },
           ]}
         >
+          {/* Добавляем type="tel", inputMode="numeric", pattern="[0-9]*" */}
           <Input.OTP
             ref={inputRef}
             onChange={handleChange}
             length={4}
             formatter={(str) => str.toUpperCase()}
             className={styles.codeInput}
+            type="tel"
+            inputMode="numeric"
+            pattern="[0-9]*"
           />
         </Form.Item>
       </Form>
     </>
   );
 }
+
+// import React, { useEffect, useRef } from "react";
+// import { Form, Input, Typography } from "antd";
+// import useAuth from "../../../../stores/useAuth";
+// import styles from "./CodeForm.module.css";
+
+// export default function CodeForm() {
+//   const { verifyPincode } = useAuth();
+//   const formRef = useRef(null);
+//   const inputRef = useRef(null);
+
+//   useEffect(() => {
+//     inputRef.current.focus();
+//   }, []);
+
+//   const onFinish = (values) => {
+//     const pincode = values.otp;
+//     verifyPincode(pincode);
+//   };
+
+//   const handleChange = (value) => {
+//     if (value.length === 4) {
+//       formRef.current.submit();
+//     }
+//   };
+
+//   return (
+//     <>
+//       <Typography.Text level={5}>Введите код из СМС:</Typography.Text>
+//       <Form
+//         ref={formRef}
+//         name="codeForm"
+//         onFinish={onFinish}
+//         className={styles.codeFormContainer}
+//       >
+//         <Form.Item
+//           name="otp"
+//           rules={[
+//             { required: true, message: "Пожалуйста, введите код из СМС" },
+//           ]}
+//         >
+//           <Input.OTP
+//             ref={inputRef}
+//             onChange={handleChange}
+//             length={4}
+//             formatter={(str) => str.toUpperCase()}
+//             className={styles.codeInput}
+//           />
+//         </Form.Item>
+//       </Form>
+//     </>
+//   );
+// }
