83d033d36ed154ebf22b3b94a90f9f8c5c1c8487
Stanislav <yanut995@gmail.com>
2024-12-10 14:55:44 +0300

    Поправил баг с Незакрытием хэдера при нажатии на ?

---
+++ src/components/Global/AppHeader/AppHeaderDrawers.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { Drawer, Menu } from "antd";
+import NotificationList from "../../FormComponentsNew/Notifications/NotificationPanel";
+import { MobileExtraMenu } from "./AppHeaderMenus";
+
+export function MobileMenuDrawer({
+  menuDrawerVisible,
+  closeMenuDrawer,
+  currentPage,
+  setCurrentPage,
+  itemsMobile,
+  colorText,
+  darkMode,
+  handlerDarkMode,
+  auth,
+  profile,
+  handleLogout,
+  handlerChangeAuth,
+}) {
+  return (
+    <Drawer
+      title="Меню"
+      placement="left"
+      onClose={closeMenuDrawer}
+      open={menuDrawerVisible}
+    >
+      <Menu
+        mode="inline"
+        selectedKeys={[currentPage]}
+        onClick={({ key }) => {
+          setCurrentPage(key);
+          closeMenuDrawer();
+        }}
+        items={itemsMobile}
+      />
+      <MobileExtraMenu
+        colorText={colorText}
+        darkMode={darkMode}
+        handlerDarkMode={handlerDarkMode}
+        auth={auth}
+        profile={profile}
+        handleLogout={handleLogout}
+        handlerChangeAuth={handlerChangeAuth}
+        closeMenuDrawer={closeMenuDrawer} 
+        setCurrentPage={setCurrentPage} 
+      />
+    </Drawer>
+  );
+}
+
+export function NotificationDrawer({
+  notificationDrawerVisible,
+  closeNotificationDrawer,
+}) {
+  return (
+    <Drawer
+      title="Уведомления"
+      placement="right"
+      onClose={closeNotificationDrawer}
+      open={notificationDrawerVisible}
+    >
+      <NotificationList />
+    </Drawer>
+  );
+}
+
+// import React from "react";
+// import { Drawer, Menu } from "antd";
+// import NotificationList from "../../FormComponentsNew/Notifications/NotificationPanel";
+// import { MobileExtraMenu } from "./AppHeaderMenus";
+
+// export function MobileMenuDrawer({
+//   menuDrawerVisible,
+//   closeMenuDrawer,
+//   currentPage,
+//   setCurrentPage,
+//   itemsMobile,
+//   colorText,
+//   darkMode,
+//   handlerDarkMode,
+//   auth,
+//   profile,
+//   handleLogout,
+//   handlerChangeAuth,
+// }) {
+//   return (
+//     <Drawer
+//       title="Меню"
+//       placement="left"
+//       onClose={closeMenuDrawer}
+//       open={menuDrawerVisible}
+//     >
+//       <Menu
+//         mode="inline"
+//         selectedKeys={[currentPage]}
+//         onClick={({ key }) => {
+//           setCurrentPage(key);
+//           closeMenuDrawer();
+//         }}
+//         items={itemsMobile}
+//       />
+//       <MobileExtraMenu
+//         colorText={colorText}
+//         darkMode={darkMode}
+//         handlerDarkMode={handlerDarkMode}
+//         auth={auth}
+//         profile={profile}
+//         handleLogout={handleLogout}
+//         handlerChangeAuth={handlerChangeAuth}
+//       />
+//     </Drawer>
+//   );
+// }
+
+// export function NotificationDrawer({
+//   notificationDrawerVisible,
+//   closeNotificationDrawer,
+// }) {
+//   return (
+//     <Drawer
+//       title="Уведомления"
+//       placement="right"
+//       onClose={closeNotificationDrawer}
+//       open={notificationDrawerVisible}
+//     >
+//       <NotificationList />
+//     </Drawer>
+//   );
+// }
