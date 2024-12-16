3d7cb9f2c427ab5d265e52531d1a41523b09edee
luxeivan <luxeivan@gmail.com>
2024-12-16 19:33:27 +0300

    Сделал исправление

--- src/components/FormComponentsNew/InnInput.js
+++ src/components/FormComponentsNew/InnInput.js
@@ -27,7 +27,7 @@ export default function InnInput({
     const form = Form.useFormInstance();
     // let fieldDepends = Form.useWatch(dependOf, form);
     const [options, setOptions] = useState([])
-    const objProperties = JSON.parse(properties).externalService
+    const objProperties = properties.externalService
 
 
     const fetchSuggestions = debounce((inn) => {
