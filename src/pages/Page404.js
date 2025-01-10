614ddaf5e913e54021ce1dfd475d714178eeb6ed
Stanislav <yanut995@gmail.com>
2025-01-10 11:53:10 +0300

    Навел порядок в Services и ServicesItem

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,19 +1,23 @@
-import { Button, Result } from 'antd'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Button, Result } from "antd";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Page404() {
-    const navigator = useNavigate()
-const handlerGoHome = ()=>{
-    navigator('/')
-}
+  const navigator = useNavigate();
+  const handlerGoHome = () => {
+    navigator("/");
+  };
 
-    return (        
-        <Result
-            status="404"
-            title="404"
-            subTitle="Извините, но такой страницы не существует"
-            extra={<Button type="primary" onClick={handlerGoHome}>На главную</Button>}
-        />
-    )
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Извините, но такой страницы не существует"
+      extra={
+        <Button type="primary" onClick={handlerGoHome}>
+          На главную
+        </Button>
+      }
+    />
+  );
 }
