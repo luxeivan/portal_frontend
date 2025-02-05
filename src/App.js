3708c212e82f9ee2035f681ba9e40796e373048f
Stanislav <yanut995@gmail.com>
2025-02-05 08:55:23 +0300

    Починил биг баг

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ export default function App() {
                 </Route>
 
                 <Route path="/answers" element={<Answers />} />
-                <Route path="/test" element={<Test />} />
+                <Route path="/tests" element={<Test />} />
                 <Route path="*" element={<Page404 />} />
               </Routes>
             </Content>
