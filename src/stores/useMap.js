cec9eacda620fe544aa1ac1af417e30754789976
Stanislav <yanut995@gmail.com>
2025-02-10 16:04:50 +0300

    Добавил шрифты

--- src/stores/useMap.js
+++ src/stores/useMap.js
@@ -207,6 +207,18 @@ export const useMap = (initialValue = {}) => {
         },
       };
 
+      pdfMake.fonts = {
+        Roboto: {
+          normal:
+            "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+          bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+          italics:
+            "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+          bolditalics:
+            "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+        },
+      };
+
       pdfMake.createPdf(docDefinition).download("map.pdf");
     } catch (error) {
       console.error("Ошибка при загрузке изображения карты:", error);
