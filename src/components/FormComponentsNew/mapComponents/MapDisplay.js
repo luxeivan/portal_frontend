f4cf8e64a020235bdf3356b71d9aefcb447e5f3c
Stanislav <yanut995@gmail.com>
2025-02-11 08:38:49 +0300

    Поменял метки

--- src/components/FormComponentsNew/mapComponents/MapDisplay.js
+++ src/components/FormComponentsNew/mapComponents/MapDisplay.js
@@ -30,7 +30,7 @@ const MapDisplay = ({
             geometry={selectedPoint}
             options={{
               draggable: true,
-              preset: "islands#redIcon",
+              preset: "islands#redCircleDotIcon", 
             }}
             onDragEnd={(e) => {
               const newCoords = e.get("target").geometry.getCoordinates();
@@ -54,7 +54,7 @@ const MapDisplay = ({
                 geometry={point}
                 options={{
                   draggable: true,
-                  preset: "islands#blueIcon",
+                  preset: "islands#blueCircleDotIcon",
                 }}
                 onDragEnd={(e) => {
                   const newCoords = e.get("target").geometry.getCoordinates();
@@ -70,93 +70,3 @@ const MapDisplay = ({
 };
 
 export default MapDisplay;
-
-// import React from "react";
-// import {
-//   YMaps,
-//   Map,
-//   Placemark,
-//   Polygon,
-//   ZoomControl,
-// } from "@pbe/react-yandex-maps";
-
-// const MapDisplay = ({
-//   mapState,
-//   onClick,
-//   selectedPoint,
-//   polygonPoints,
-//   mapRef,
-// }) => {
-//   return (
-//     <YMaps>
-//       <Map
-//         state={mapState}
-//         width="100%"
-//         height="400px"
-//         onClick={onClick}
-//         instanceRef={mapRef}
-//       >
-//         <ZoomControl />
-//         {selectedPoint && <Placemark geometry={selectedPoint} />}
-//         {polygonPoints?.length > 0 && (
-//           <Polygon
-//             geometry={[polygonPoints]}
-//             options={{
-//               fillColor: "rgba(0, 0, 255, 0.3)",
-//               strokeColor: "#0000FF",
-//               strokeWidth: 2,
-//             }}
-//           />
-//         )}
-//       </Map>
-//     </YMaps>
-//   );
-// };
-
-// export default MapDisplay;
-
-// import React from "react";
-// import {
-//   YMaps,
-//   Map,
-//   Placemark,
-//   Polygon,
-//   ZoomControl,
-// } from "@pbe/react-yandex-maps";
-
-// const MapDisplay = ({
-//   mapState,
-//   onBoundsChange,
-//   onClick,
-//   selectedPoint,
-//   polygonPoints,
-//   mapRef,
-// }) => {
-//   return (
-//     <YMaps>
-//       <Map
-//         state={mapState}
-//         width="100%"
-//         height="400px"
-//         onClick={onClick}
-//         onBoundsChange={onBoundsChange}
-//         instanceRef={mapRef}
-//       >
-//         <ZoomControl />
-//         {selectedPoint && <Placemark geometry={selectedPoint} />}
-//         {polygonPoints.length > 0 && (
-//           <Polygon
-//             geometry={[polygonPoints]}
-//             options={{
-//               fillColor: "rgba(0, 0, 255, 0.3)",
-//               strokeColor: "#0000FF",
-//               strokeWidth: 2,
-//             }}
-//           />
-//         )}
-//       </Map>
-//     </YMaps>
-//   );
-// };
-
-// export default MapDisplay;
