1f16d6e3c8dec4703404cf47742c8980056e9f70
luxeivan <luxeivan@gmail.com>
2025-02-20 11:11:14 +0300

    Начал работу с зависимостями для запросов dadata

--- src/pages/Cabinet/NewClaim.js
+++ src/pages/Cabinet/NewClaim.js
@@ -91,7 +91,7 @@ export default function NewClaim() {
       event.preventDefault();
     }
   };
-
+console.log(serviceItem)
   return (
     <div style={{ maxWidth: "100%", margin: "0 auto" }}>
       <AppHelmet
